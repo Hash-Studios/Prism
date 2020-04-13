1f7b92e891daddbd0e77a089a55fd4c51c309ff7
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-04-14 00:36:17 +0530

    Minor UI changes, cached images

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
 
@@ -8,14 +9,24 @@ class Feed extends StatefulWidget {
 }
 
 class _FeedState extends State<Feed> {
+
+  void getimage() async {
+    var file = await DefaultCacheManager().getSingleFile("https://46.media.tumblr.com/85b3585957f0c810bf7f1f09dd8c7fe7/tumblr_p14hllwA221uzwgsuo1_400.gif");
+    print(file.path.toString());
+  }
+
+
   @override
   void initState() {
     super.initState();
+    getimage();
+
+    
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
+    return DefaultTabController(initialIndex: 0,
       length: 4,
       child: Scaffold(
         key: _scaffoldKey,
@@ -53,7 +64,7 @@ class _FeedState extends State<Feed> {
                 ),
                 decoration: new BoxDecoration(
                   image: new DecorationImage(
-                    image: new NetworkImage('https://picsum.photos/600/600'),
+                    image: new NetworkImage('https://46.media.tumblr.com/85b3585957f0c810bf7f1f09dd8c7fe7/tumblr_p14hllwA221uzwgsuo1_400.gif'),
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -61,43 +72,44 @@ class _FeedState extends State<Feed> {
                   child: Icon(
                     Icons.format_paint,
                     size: 48,
+                    color: Colors.white70,
                   ),
-                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage("https://46.media.tumblr.com/85b3585957f0c810bf7f1f09dd8c7fe7/tumblr_p14hllwA221uzwgsuo1_400.gif"),
                 ),
               ),
               new ListTile(
-                  leading: Icon(Icons.format_paint),
+                  leading: Icon(Icons.format_paint,color: Colors.deepPurple,),
                   title: new Text("Wallpapers"),
                   onTap: () {
                     Navigator.pop(context);
                   }),
               new ListTile(
-                  leading: Icon(Icons.photo),
+                  leading: Icon(Icons.photo,color: Colors.green,),
                   title: new Text("Photographs"),
                   onTap: () {
                     Navigator.pop(context);
                   }),
               new ListTile(
-                  leading: Icon(Icons.favorite),
+                  leading: Icon(Icons.favorite,color: Colors.pink,),
                   title: new Text("Favourites"),
                   onTap: () {
                     Navigator.pop(context);
                   }),
               new ListTile(
-                  leading: Icon(Icons.arrow_downward),
+                  leading: Icon(Icons.arrow_downward,color: Colors.blue,),
                   title: new Text("Downloads"),
                   onTap: () {
                     Navigator.pop(context);
                   }),
               new ListTile(
-                  leading: Icon(Icons.settings),
+                  leading: Icon(Icons.settings,color: Colors.deepOrange,),
                   title: new Text("Settings"),
                   onTap: () {
                     Navigator.pop(context);
                   }),
               new Divider(),
               new ListTile(
-                  leading: Icon(Icons.info),
+                  leading: Icon(Icons.info,color: Colors.amber[700],),
                   title: new Text("About"),
                   onTap: () {
                     Navigator.pop(context);
@@ -108,16 +120,16 @@ class _FeedState extends State<Feed> {
         body: TabBarView(
           children: [
             new Container(
-              color: Colors.white,
+              color: Colors.deepPurple,
             ),
             new Container(
-              color: Colors.grey,
+              color: Colors.green,
             ),
             new Container(
-              color: Colors.white,
+              color: Colors.pink,
             ),
             new Container(
-              color: Colors.grey,
+              color: Colors.blue,
             ),
           ],
         ),
@@ -141,6 +153,7 @@ class _FeedState extends State<Feed> {
           indicatorSize: TabBarIndicatorSize.label,
           indicatorPadding: EdgeInsets.all(5.0),
           indicatorColor: Colors.grey,
+
         ),
       ),
     );
