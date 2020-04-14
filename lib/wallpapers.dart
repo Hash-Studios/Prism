46911dd011a5f09f82cf46908273637c380eb0ec
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-04-14 14:08:43 +0530

    Added Scrollbar, refresh indicator, lazy loading to feed

--- lib/wallpapers.dart
+++ lib/wallpapers.dart
@@ -8,64 +8,106 @@ class Wallpapers extends StatefulWidget {
 }
 
 class _WallpapersState extends State<Wallpapers> {
+  Future<Null> refreshList() async {
+    refreshKey.currentState?.show(atTop: false);
+    await Future.delayed(Duration(seconds: 2));
+
+    setState(() {
+      items = List.generate(
+          20, (number) => "https://picsum.photos/id/${number + 5}/300/400");
+    });
+
+    return null;
+  }
+
+  ScrollController controller;
+  List<String> items =
+      List.generate(20, (number) => "https://picsum.photos/id/$number/300/400");
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+
   @override
   void initState() {
     super.initState();
+    controller = new ScrollController()..addListener(_scrollListener);
     // ScreenUtil.init(context, width: 720, height: 1440, allowFontScaling: true);
   }
 
   @override
+  void dispose() {
+    controller.removeListener(_scrollListener);
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    // print(controller.position.extentAfter);
+    if (controller.position.extentAfter < 200) {
+      setState(() {
+        items.addAll(new List.generate(
+            10, (number) => "https://picsum.photos/id/${number + 40}/300/400"));
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return new Container(
-        color: Colors.white,
-        child: GridView.builder(
-            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            itemCount: 10,
-            gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, childAspectRatio: 0.75),
-            itemBuilder: (BuildContext context, int index) {
-              return new GestureDetector(
-                child: new Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(24))),
-                  elevation: 8.0,
-                  semanticContainer: true,
-                  margin: EdgeInsets.all(6),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: new Container(
-                      child: new Image(
-                    image: CacheImage(
-                      "https://picsum.photos/300/400",
+    return RefreshIndicator(
+      onRefresh: refreshList,
+      child: new Container(
+          color: Colors.white,
+          child: Scrollbar(
+            child: GridView.builder(
+                controller: controller,
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                itemCount: items.length,
+                gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, childAspectRatio: 0.75),
+                itemBuilder: (BuildContext context, int index) {
+                  return new GestureDetector(
+                    child: new Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(24))),
+                      elevation: 8.0,
+                      semanticContainer: true,
+                      margin: EdgeInsets.all(6),
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      child: new Container(
+                          child: new FadeInImage(
+                        placeholder: CacheImage(
+                            "https://via.placeholder.com/300x400.jpg/FFFFFF/FFFFFF"),
+                        image: CacheImage(
+                          items[index],
+                        ),
+                        fit: BoxFit.cover,
+                      )),
                     ),
-                    fit: BoxFit.fill,
-                  )),
-                ),
-                onTap: () {
-                  showDialog(
-                    barrierDismissible: false,
-                    context: context,
-                    child: new AlertDialog(
-                      title: new Column(
-                        children: <Widget>[
-                          new Text("GridView"),
-                          new Icon(
-                            Icons.favorite,
-                            color: Colors.green,
+                    onTap: () {
+                      showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        child: new AlertDialog(
+                          title: new Column(
+                            children: <Widget>[
+                              new Text("GridView"),
+                              new Icon(
+                                Icons.favorite,
+                                color: Colors.green,
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      content: new Text("Selected Item $index"),
-                      actions: <Widget>[
-                        new FlatButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: new Text("OK"))
-                      ],
-                    ),
+                          content: new Text("Selected Item $index"),
+                          actions: <Widget>[
+                            new FlatButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: new Text("OK"))
+                          ],
+                        ),
+                      );
+                    },
                   );
-                },
-              );
-            }));
+                }),
+          )),
+    );
   }
 }
