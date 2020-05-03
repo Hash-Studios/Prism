dfd5b3be58cd5fa0def87236ba54d88e34cf8684
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-05-03 23:18:49 +0530

    Search page done

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Prism',
             theme: theme,
-            home: Feed(),
+            home: LoginScreen3(),
           );
         });
   }
