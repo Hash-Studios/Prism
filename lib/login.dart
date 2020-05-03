d12ddb77da09d842041a0ef3015e7d4744a78eb5
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-05-04 01:31:21 +0530

    separated sign in check and login screen

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,160 +7,11 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:wallpapers_app/feed.dart';
-
-class LoginScreen3 extends StatefulWidget {
-  @override
-  _LoginScreen3State createState() => new _LoginScreen3State();
-}
-
-class _LoginScreen3State extends State<LoginScreen3>
-    with TickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  // Widget HomePage() {
-  //   return Scaffold(
-  //     body: new Container(
-  //       height: MediaQuery.of(context).size.height,
-  //       decoration: BoxDecoration(
-  //         color: Colors.blueAccent[100],
-  //         image: DecorationImage(
-  //           colorFilter: new ColorFilter.mode(
-  //               Colors.black.withOpacity(0.1), BlendMode.dstATop),
-  //           image: AssetImage('assets/images/Bg-Blue.jpg'),
-  //           fit: BoxFit.cover,
-  //         ),
-  //       ),
-  //       child: new Column(
-  //         children: <Widget>[
-  //           Container(
-  //             padding: EdgeInsets.only(top: 250.0),
-  //             child: Center(
-  //               child: Icon(
-  //                 Icons.chat,
-  //                 color: Colors.white,
-  //                 size: 40.0,
-  //               ),
-  //             ),
-  //           ),
-  //           Container(
-  //             padding: EdgeInsets.only(top: 20.0),
-  //             child: Text(
-  //               "ChatMe",
-  //               style: GoogleFonts.montserrat(
-  //                 color: Colors.white,
-  //                 fontSize: 50,
-  //               ),
-  //             ),
-  //           ),
-  //           new Container(
-  //             width: MediaQuery.of(context).size.width,
-  //             margin:
-  //                 const EdgeInsets.only(left: 60.0, right: 60.0, top: 200.0),
-  //             alignment: Alignment.center,
-  //             child: new Row(
-  //               children: <Widget>[
-  //                 new Expanded(
-  //                   child: new FlatButton(
-  //                     shape: new RoundedRectangleBorder(
-  //                         borderRadius: new BorderRadius.circular(30.0)),
-  //                     color: Colors.white70,
-  //                     onPressed: () => gotoSignup(),
-  //                     splashColor: Colors.blueAccent[100],
-  //                     child: new Container(
-  //                       padding: const EdgeInsets.symmetric(
-  //                         vertical: 20.0,
-  //                         horizontal: 20.0,
-  //                       ),
-  //                       child: new Row(
-  //                         mainAxisAlignment: MainAxisAlignment.center,
-  //                         children: <Widget>[
-  //                           new Expanded(
-  //                             child: Text(
-  //                               "Start Chatting",
-  //                               textAlign: TextAlign.center,
-  //                               style: TextStyle(
-  //                                   color: Colors.blueAccent[100],
-  //                                   fontWeight: FontWeight.bold,
-  //                                   fontSize: 18),
-  //                             ),
-  //                           ),
-  //                         ],
-  //                       ),
-  //                     ),
-  //                   ),
-  //                 ),
-  //               ],
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
-
-  // gotoSignup() {
-  //   //controller_minus1To0.reverse(from: 0.0);
-  //   _controller.animateToPage(
-  //     2,
-  //     duration: Duration(milliseconds: 800),
-  //     curve: Curves.bounceOut,
-  //   );
-  // }
-
-  // PageController _controller =
-  //     new PageController(initialPage: 0, viewportFraction: 1.0);
-  SharedPreferences prefs;
-  Future<String> getLogin() async {
-    prefs = await SharedPreferences.getInstance();
-    return prefs.getString('isLoggedIn');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return
-        // Container(
-        //     height: MediaQuery.of(context).size.height,
-        //     child:
-        // child: PageView(
-        //   controller: _controller,
-        //   physics: new AlwaysScrollableScrollPhysics(),
-        //   children: getLogin() == null
-        //       ? <Widget>[
-        //           HomePage(),
-        //           LoginScreen(
-        //             title: "ChatMe",
-        //           )
-        //         ]
-        //       : getLogin() == true
-        //           ? <Widget>[
-        //               LoginScreen(
-        //                 title: "ChatMe",
-        //               )
-        //             ]
-        //           : <Widget>[
-        //               HomePage(),
-        LoginScreen(
-      title: "Prism",
-      // )
-      //             ],
-      //   scrollDirection: Axis.horizontal,
-      // )
-    );
-  }
-}
-
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key, this.title}) : super(key: key);
-
   final String title;
-
   @override
   LoginScreenState createState() => LoginScreenState();
 }
@@ -180,36 +30,7 @@ class LoginScreenState extends State<LoginScreen> {
   @override
   void initState() {
     super.initState();
-    isSignedIn();
   }
-
-  Future delay() async {
-    await new Future.delayed(new Duration(milliseconds: 1500), () {
-      handleSignIn();
-    });
-  }
-
-  void isSignedIn() async {
-    this.setState(() {
-      isLoading = true;
-    });
-
-    prefs = await SharedPreferences.getInstance();
-
-    isLoggedIn = await googleSignIn.isSignedIn();
-    if (isLoggedIn) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => Feed(currentUserId: prefs.getString('id'))),
-      );
-    }
-
-    this.setState(() {
-      isLoading = false;
-    });
-  }
-
   Future<Null> handleSignIn() async {
     prefs = await SharedPreferences.getInstance();
 
@@ -222,7 +43,7 @@ class LoginScreenState extends State<LoginScreen> {
       googleAuth = await googleUser.authentication;
     } catch (e) {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => LoginScreen3()));
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
     }
     final AuthCredential credential = GoogleAuthProvider.getCredential(
       accessToken: googleAuth.accessToken,
@@ -231,43 +52,29 @@ class LoginScreenState extends State<LoginScreen> {
     FirebaseUser firebaseUser =
         (await firebaseAuth.signInWithCredential(credential)).user;
     if (firebaseUser != null) {
-      // Check is already sign up
       final QuerySnapshot result = await Firestore.instance
           .collection('users')
           .where('id', isEqualTo: firebaseUser.uid)
           .getDocuments();
       final List<DocumentSnapshot> documents = result.documents;
       if (documents.length == 0) {
-        // Update data to server if new user
         Firestore.instance
             .collection('users')
             .document(firebaseUser.uid)
             .setData({
           'name': firebaseUser.displayName,
           'email': firebaseUser.email,
-          // 'photoUrl': firebaseUser.photoUrl,
           'id': firebaseUser.uid,
-          // 'createdAt': DateTime.now().millisecondsSinceEpoch.toString(),
-          // 'chattingWith': null,
-          // 'online' : false,
-          // 'lastSeen': DateTime.now().millisecondsSinceEpoch.toString(),
         });
-        // Firestore.instance.collection('users').document(firebaseUser.uid).collection('images').
-
-        // Write data to local
         currentUser = firebaseUser;
         await prefs.setString('id', currentUser.uid);
         await prefs.setString('name', currentUser.displayName);
         await prefs.setString('email', currentUser.email);
-        // await prefs.setString('photoUrl', currentUser.photoUrl);
         await prefs.setString('logged', isLoggedIn.toString());
       } else {
-        // Write data to local
         await prefs.setString('id', documents[0]['id']);
         await prefs.setString('name', documents[0]['name']);
         await prefs.setString('email', documents[0]['email']);
-        // await prefs.setString('photoUrl', documents[0]['photoUrl']);
-        // await prefs.setString('aboutMe', documents[0]['aboutMe']);
         await prefs.setString('logged', isLoggedIn.toString());
       }
       Fluttertoast.showToast(msg: "Sign in success");
@@ -344,8 +151,6 @@ class LoginScreenState extends State<LoginScreen> {
                 ],
               ),
             ),
-
-            // Loading
             Positioned(
               child: isLoading
                   ? Container(
