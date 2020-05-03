8e1eb276060ebd4f9c15aae8e8067a00d3f7f1cc
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-05-04 01:31:21 +0530

    separated sign in check and login screen

---
+++ lib/firstscreen.dart
@@ -0,0 +1,75 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:wallpapers_app/feed.dart';
+import 'package:wallpapers_app/login.dart';
+
+class LoginScreen3 extends StatefulWidget {
+  @override
+  _LoginScreen3State createState() => new _LoginScreen3State();
+}
+
+class _LoginScreen3State extends State<LoginScreen3> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  SharedPreferences prefs;
+  bool isLoading = false;
+  bool isLoggedIn = false;
+  GoogleSignInAccount googleUser;
+  GoogleSignInAuthentication googleAuth;
+
+  @override
+  void initState() {
+    super.initState();
+    isSignedIn();
+  }
+
+  void isSignedIn() async {
+    this.setState(() {
+      isLoading = true;
+    });
+
+    prefs = await SharedPreferences.getInstance();
+
+    isLoggedIn = await googleSignIn.isSignedIn();
+    if (isLoggedIn) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Feed(currentUserId: prefs.getString('id'))),
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    }
+
+    this.setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          color: DynamicTheme.of(context).data.primaryColor,
+        ),
+        Positioned(
+          child: isLoading
+              ? Container(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  color: Colors.white.withOpacity(0.8),
+                )
+              : Container(),
+        ),
+      ],
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wallpapers_app/feed.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
-import 'package:wallpapers_app/login.dart';
+import 'package:wallpapers_app/firstscreen.dart';
 import './themes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
