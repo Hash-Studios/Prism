05aed771876ddb0e2d171644dabac37bc8dd0e2b
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-05-03 23:18:49 +0530

    Search page done

--- lib/search.dart
+++ lib/search.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wallpapers_app/wallpapers.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
-import 'package:cache_image/cache_image.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:wallpapers_app/wallheaven.dart';
-import 'package:wallpapers_app/display.dart';
-import 'package:flare_flutter/flare_actor.dart';
-import 'package:flare_flutter/flare_controls.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -19,35 +13,97 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: Hero(
-          tag: 'search',
-          child: Card(
-            elevation: 0,
-            color: Colors.transparent,
-            child: IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
+        appBar: AppBar(
+          elevation: 0,
+          leading: Hero(
+            tag: 'search',
+            child: Card(
+              elevation: 0,
+              color: Colors.transparent,
+              child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+            ),
           ),
-        ),
-        title: TextField(
-          onSubmitted: (String query) {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return Wallpapers(query);
-            }));
-          },
-          decoration: InputDecoration(
-            hintText: "Search Wallpapers",
-            border: InputBorder.none,
-            hintStyle: TextStyle(
-              color: DynamicTheme.of(context).data.secondaryHeaderColor,
+          title: TextField(
+            autofocus: true,
+            onSubmitted: (String query) {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return Scaffold(
+                    resizeToAvoidBottomInset: false,
+                    appBar: AppBar(
+                      elevation: 0,
+                      centerTitle: true,
+                      title: Text(
+                        query.toLowerCase(),
+                        style: GoogleFonts.pacifico(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 30,
+                            color: DynamicTheme.of(context)
+                                .data
+                                .secondaryHeaderColor),
+                      ),
+                    ),
+                    body: Wallpapers(query));
+              }));
+            },
+            decoration: InputDecoration(
+              hintText: "Search Wallpapers",
+              border: InputBorder.none,
+              hintStyle: TextStyle(
+                color: DynamicTheme.of(context).data.secondaryHeaderColor,
+              ),
             ),
           ),
         ),
-      ),
-    );
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: DynamicTheme.of(context).data.primaryColor ==
+                          Color(0xFFFFFFFF)
+                      ? AssetImage("assets/images/loadingsw.png")
+                      : DynamicTheme.of(context).data.primaryColor ==
+                              Color(0xFF272727)
+                          ? AssetImage("assets/images/loadingdb.png")
+                          : DynamicTheme.of(context).data.primaryColor ==
+                                  Color(0xFF000000)
+                              ? AssetImage("assets/images/loadingab.png")
+                              : DynamicTheme.of(context).data.primaryColor ==
+                                      Color(0xFF263238)
+                                  ? AssetImage("assets/images/loadingcd.png")
+                                  : AssetImage("assets/images/loadingmc.png"),
+                  height: 600.h,
+                  width: 600.w,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    "Search",
+                    style: GoogleFonts.raleway(
+                        fontSize: 30,
+                        color:
+                            DynamicTheme.of(context).data.secondaryHeaderColor),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Text(
+                  "Type any keyword in the search\nbar to find your next\nfavourite wallpaper.",
+                  style: GoogleFonts.raleway(
+                      fontSize: 16,
+                      color:
+                          DynamicTheme.of(context).data.secondaryHeaderColor),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
