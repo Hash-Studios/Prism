df36a05e3c76924e2c57514bcf0e62bfe63bb66e
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-22 00:12:28 +0530

    added bottom nav bar in homescreen

--- lib/ui/pages/homeScreen.dart
+++ lib/ui/pages/homeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:Prism/ui/widgets/bottomNavBar.dart';
 import 'package:Prism/ui/widgets/categoriesBar.dart';
 import 'package:Prism/ui/widgets/gridLoader.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,11 @@ class MainWidget extends StatelessWidget {
           child: CategoriesBar(),
           preferredSize: Size(double.infinity, 60),
         ),
-        body: GridLoader());
+        body: Stack(alignment: Alignment.bottomCenter,
+          children: [
+            GridLoader(),
+            Positioned(bottom: 10, child: BottomNavBar())
+          ],
+        ));
   }
 }
