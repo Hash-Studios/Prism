c916265582bd9bbc06bd3a98e8135fc7204304b2
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-21 20:08:54 +0530

    Updated categories bar

--- lib/ui/widgets/categoriesBar.dart
+++ lib/ui/widgets/categoriesBar.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CategoriesBar extends StatelessWidget {
-  const CategoriesBar({
+  CategoriesBar({
     Key key,
-    @required this.categories,
   }) : super(key: key);
 
-  final List<String> categories;
+  final List<String> categories = [
+    'Home',
+    'Abstract',
+    'Nature',
+    'Comics',
+    'Cars',
+    'Community'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +32,23 @@ class CategoriesBar extends StatelessWidget {
               alignment: Alignment.center,
               child: Stack(
                 children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(500),
-                      color: index == 0
-                          ? Theme.of(context).accentColor
-                          : Theme.of(context).hintColor,
-                    ),
-                    margin: EdgeInsets.all(5),
-                    padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: Text(categories[index],
-                        style: index == 0
-                            ? Theme.of(context)
-                                .textTheme
-                                .headline4
-                                .copyWith(color: Theme.of(context).primaryColor)
-                            : Theme.of(context).textTheme.headline4),
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: ActionChip(
+                        pressElevation: 5,
+                        padding: EdgeInsets.fromLTRB(14, 11, 14, 11),
+                        backgroundColor: index == 0
+                            ? Theme.of(context).accentColor
+                            : Theme.of(context).hintColor,
+                        label: Text(categories[index],
+                            style: index == 0
+                                ? Theme.of(context)
+                                    .textTheme
+                                    .headline4
+                                    .copyWith(
+                                        color: Theme.of(context).primaryColor)
+                                : Theme.of(context).textTheme.headline4),
+                        onPressed: () {}),
                   ),
                 ],
               ),
