0ab531965ef38139510130b2309920874a2d4ddc
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-21 12:57:03 +0530

    New app initialised

---
+++ Prism v1.0/main.dart
@@ -0,0 +1,4 @@
+import 'package:Prism/data/notifications.dart';
+import 'package:flutter/material.dart';
+void main() => runApp(NotificationHandler());
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,31 @@
-import 'package:Prism/data/notifications.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
-void main() => runApp(NotificationHandler());
 
+void main() {
+  SplashScreen.navigate(
+    name: 'Prism Splash.flr',
+    next: (_) => MyApp(),
+    until: () => Future.delayed(Duration(seconds: 3)),
+    startAnimation: 'Main',
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
