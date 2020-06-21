f8ea2eeb3c9ccdc2fa3ce7152d6641bd4807277e
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-21 18:01:35 +0530

    added pexel provider

---
+++ lib/data/pexels/model/wallpaperp.dart
@@ -0,0 +1,18 @@
+class WallPaperP {
+  final String id;
+  final String width;
+  final String height;
+  final String url;
+  final String photographer;
+  final Map<String, String> src;
+  final int current_page;
+  WallPaperP({
+    this.id,
+    this.width,
+    this.height,
+    this.url,
+    this.photographer,
+    this.src,
+    this.current_page,
+  });
+}
---
+++ lib/data/pexels/provider/pexels.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:Prism/data/pexels/model/wallpaperp.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PexelsProvider extends ChangeNotifier {
+  List<WallPaperP> wallsP = [];
+  int pageGetDataP = 1;
+  int pageGetQueryP = 1;
+  void getDataP() async {
+    http.get(
+        "https://api.pexels.com/v1/curated?per_page=24&page=${this.pageGetDataP}",
+        headers: {
+          "Authorization":
+              "563492ad6f9170000100000107272bb5cab346b68e27263fbf1b6a72"
+        }).then(
+      (http.Response response) {
+        var resp = json.decode(response.body);
+        for (int i = 0; i < resp.length; i++) {
+          this.wallsP.add(
+                WallPaperP(
+                    id: resp["photos"][i]["id"],
+                    url: resp["photos"][i]["url"],
+                    width: resp["photos"][i]["width"],
+                    height: resp["photos"][i]["height"],
+                    photographer: resp["photos"][i]["photographer"],
+                    src: resp["photos"][i]["category"],
+                    current_page: resp["page"]),
+              );
+        }
+        this.pageGetDataP = int.parse(resp["page"]) + 1;
+      },
+    );
+    notifyListeners();
+  }
+
+  Future<WallPaperP> getWallbyIDP(String id) async {
+    http.get("https://api.pexels.com/v1/photos/$id").then(
+      (http.Response response) {
+        var resp = json.decode(response.body)["data"];
+        return WallPaperP(
+            id: resp["id"],
+            url: resp["url"],
+            width: resp["width"],
+            height: resp["height"],
+            photographer: resp["photographer"],
+            src: resp["category"]);
+      },
+    );
+  }
+
+  void getWallsPbyQuery(String query) async {
+    http
+        .get(
+            "https://api.pexels.com/v1/search?query=$query&per_page=80&page=${this.pageGetQueryP}")
+        .then(
+      (http.Response response) {
+        var resp = json.decode(response.body);
+        for (int i = 0; i < resp.length; i++) {
+          this.wallsP.add(
+                WallPaperP(
+                    id: resp["photos"][i]["id"],
+                    url: resp["photos"][i]["url"],
+                    width: resp["photos"][i]["width"],
+                    height: resp["photos"][i]["height"],
+                    photographer: resp["photos"][i]["photographer"],
+                    src: resp["photos"][i]["category"],
+                    current_page: resp["page"]),
+              );
+        }
+        this.pageGetQueryP = int.parse(resp["page"]) + 1;
+      },
+    );
+    notifyListeners();
+  }
+}
