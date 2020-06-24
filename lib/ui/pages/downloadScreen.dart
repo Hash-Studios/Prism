02b7f86294d3394a12ab1c8f5721d835fe0e19a8
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-24 19:01:05 +0530

    added downloadscreen

--- lib/ui/pages/downloadScreen.dart
+++ lib/ui/pages/downloadScreen.dart
@@ -1,8 +1,163 @@
+import 'dart:collection';
+import 'dart:io';
+
+import 'package:Prism/routing_constants.dart';
+import 'package:Prism/theme/themeModel.dart';
 import 'package:flutter/material.dart';
 
-class DownloadScreen extends StatelessWidget {
+class DownloadScreen extends StatefulWidget {
+  @override
+  _DownloadScreenState createState() => _DownloadScreenState();
+}
+
+class _DownloadScreenState extends State<DownloadScreen>
+    // with SingleTickerProviderStateMixin 
+    {
+  // AnimationController _controller;
+  // Animation<Color> animation;
+  bool dataFetched = false;
+  Map<dynamic, dynamic> allImageInfo = HashMap();
+  List files = List();
+  ScrollController controller;
+  var refreshDownloadKey = GlobalKey<RefreshIndicatorState>();
+  @override
+  void initState() {
+    super.initState();
+    dataFetched = false;
+    files = [];
+    readData();
+    // _controller = AnimationController(
+    //   duration: const Duration(milliseconds: 800),
+    //   vsync: this,
+    // );
+    // animation = ThemeModel().returnTheme() == ThemeType.Dark
+    //     ? TweenSequence<Color>(
+    //         [
+    //           TweenSequenceItem(
+    //             weight: 1.0,
+    //             tween: ColorTween(
+    //               begin: Colors.white10,
+    //               end: Colors.white12,
+    //             ),
+    //           ),
+    //           TweenSequenceItem(
+    //             weight: 1.0,
+    //             tween: ColorTween(
+    //               begin: Colors.white12,
+    //               end: Colors.white10,
+    //             ),
+    //           ),
+    //         ],
+    //       ).animate(_controller)
+    //     : TweenSequence<Color>(
+    //         [
+    //           TweenSequenceItem(
+    //             weight: 1.0,
+    //             tween: ColorTween(
+    //               begin: Colors.black.withOpacity(.1),
+    //               end: Colors.black.withOpacity(.14),
+    //             ),
+    //           ),
+    //           TweenSequenceItem(
+    //             weight: 1.0,
+    //             tween: ColorTween(
+    //               begin: Colors.black.withOpacity(.14),
+    //               end: Colors.black.withOpacity(.1),
+    //             ),
+    //           ),
+    //         ],
+    //       ).animate(_controller)
+    //   ..addListener(() {
+    //     setState(() {});
+    //   });
+    // _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    // _controller?.dispose();
+    super.dispose();
+  }
+
+  Future<String> get localfile async {
+    String path = 'storage/emulated/0/';
+    return '$path/Prism';
+  }
+
+  Future<void> readData() async {
+    final file = await localfile;
+    files = Directory(file).listSync();
+
+    if (files.length == 0) {
+      setState(() {
+        dataFetched = false;
+      });
+    } else {
+      setState(() {
+        dataFetched = true;
+      });
+    }
+  }
+
+  Future<Null> refreshList() async {
+    refreshDownloadKey.currentState?.show(atTop: true);
+    await Future.delayed(Duration(milliseconds: 500));
+    setState(() {
+      files = [];
+      dataFetched = false;
+    });
+    readData();
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+          child: RefreshIndicator(
+        key: refreshDownloadKey,
+        onRefresh: refreshList,
+        child: Container(
+          child: GridView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.fromLTRB(4, 0, 4, 4),
+            itemCount: files.length,
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent:
+                    MediaQuery.of(context).orientation == Orientation.portrait
+                        ? 300
+                        : 250,
+                childAspectRatio: 0.830,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8),
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                child: Container(
+                  decoration: files.length == 0
+                      ? BoxDecoration(
+                          color: Theme.of(context).accentColor.withOpacity(0.12),
+                          borderRadius: BorderRadius.circular(20),
+                        )
+                      : BoxDecoration(
+                          color: Theme.of(context).accentColor.withOpacity(0.12),
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                              image: FileImage(files[index]), fit: BoxFit.cover)),
+                ),
+                onTap: () {
+                  //     Navigator.pushNamed(context, WallpaperRoute, arguments: [
+                  //   "Downloads",
+                  //   index,
+                  //   Provider.of<WallHavenProvider>(context, listen: false)
+                  //       .walls[index]
+                  //       .thumbs["small"],
+                  // ]);
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
