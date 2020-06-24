a9643b4811d76912815a33653004b44154431f67
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-24 18:13:23 +0530

    added progressbar in downloadButton

--- lib/ui/widgets/downloadButton.dart
+++ lib/ui/widgets/downloadButton.dart
@@ -3,12 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:Prism/theme/toasts.dart' as toasts;
 import 'package:gallery_saver/gallery_saver.dart';
 
-class DownloadButton extends StatelessWidget {
+class DownloadButton extends StatefulWidget {
   final String link;
   const DownloadButton({
     @required this.link,
     Key key,
   }) : super(key: key);
+
+  @override
+  _DownloadButtonState createState() => _DownloadButtonState();
+}
+
+class _DownloadButtonState extends State<DownloadButton> {
+  bool isLoading;
+  @override
+  void initState() {
+    isLoading = false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,33 +29,52 @@ class DownloadButton extends StatelessWidget {
         print("Download");
         onDownload();
       },
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).primaryColor,
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(.25),
-                blurRadius: 4,
-                offset: Offset(0, 4))
-          ],
-          borderRadius: BorderRadius.circular(500),
-        ),
-        padding: EdgeInsets.all(17),
-        child: Icon(
-          JamIcons.download,
-          color: Theme.of(context).accentColor,
-          size: 30,
-        ),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(.25),
+                    blurRadius: 4,
+                    offset: Offset(0, 4))
+              ],
+              borderRadius: BorderRadius.circular(500),
+            ),
+            padding: EdgeInsets.all(17),
+            child: Icon(
+              JamIcons.download,
+              color: Theme.of(context).accentColor,
+              size: 30,
+            ),
+          ),
+          Positioned(
+              top: 0,
+              left: 0,
+              height: 63,
+              width: 63,
+              child: isLoading ? CircularProgressIndicator() : Container())
+        ],
       ),
     );
   }
 
   void onDownload() async {
+    setState(() {
+      isLoading = true;
+    });
     toasts.startDownload();
-    GallerySaver.saveImage(link, albumName: "Prism").then((value) {
+    GallerySaver.saveImage(widget.link, albumName: "Prism").then((value) {
       toasts.completedDownload();
+      setState(() {
+        isLoading = false;
+      });
     }).catchError((e) {
       toasts.error(e.toString());
+      setState(() {
+        isLoading = false;
+      });
     });
   }
 }
