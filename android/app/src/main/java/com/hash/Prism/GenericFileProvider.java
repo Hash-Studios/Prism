06d71bdf2b78452a3971f2c39336b071d325eeb9
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-03 17:26:25 +0530

    added crop functionality

---
+++ android/app/src/main/java/com/hash/Prism/GenericFileProvider.java
@@ -0,0 +1,5 @@
+package com.hash.prism;
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {}
--- android/app/src/main/java/com/hash/Prism/MainActivity.java
+++ android/app/src/main/java/com/hash/Prism/MainActivity.java
@@ -1,8 +1,9 @@
 package com.hash.prism;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
+import androidx.core.content.FileProvider;
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 
 import com.bumptech.glide.request.target.CustomTarget;
@@ -10,21 +11,34 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 import android.app.WallpaperManager;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.util.Pair;
-
+import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.transition.Transition;
-
+import com.theartofdev.edmodo.cropper.CropImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "flutter.prism.set_wallpaper";
-    public static  MethodChannel.Result res;
+    public static MethodChannel.Result res;
+    private static int METHOD = -1;
+    private static Uri URI = null;
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
@@ -34,118 +48,178 @@ public class MainActivity extends FlutterActivity {
                         (call, result) -> {
 
                             res = result;
-                            if (call.method.equals("set_lock_wallpaper")){
 
-                                String url = call.argument("url"); // .argument returns the correct type
-                                android.util.Log.i("Arguments ", "configureFlutterEngine: "+url);
+                            switch (call.method) {
+                                case "set_lock_wallpaper": {
+                                    METHOD = 1;
+                                    String url = call.argument("url"); // .argument returns the correct type
 
-                                Glide.with(getActivity())
-                                        .asBitmap()
-                                        .load(url)
-                                        .into(new CustomTarget<Bitmap>() {
-                                            @Override
-                                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                                                Log.i("Arguments ", "configureFlutterEngine: "+"Ready 1");
-                                                SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity());
-                                                setWallPaperTask.execute(new Pair(resource,"1"));
-//                                                WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
-//                                                try {
-//                                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                                                        wallpaperManager.setBitmap(resource,null,true,WallpaperManager.FLAG_LOCK);
-//                                                    }
-//                                                    // Intent intent = new Intent("com.android.camera.action.CROP");
-//                                                    // intent.setType("image/*");
-//                                                    // Toast.makeText(this, "Wallpaper set!", Toast.LENGTH_SHORT).show();
-////                                                    Intent wall_intent =  new Intent(Intent.ACTION_ATTACH_DATA);
-////                                                    wall_intent.setDataAndType(Uri.parse(url), "image/*");
-////                                                    wall_intent.putExtra("mimeType", "image/*");
-////                                                    Intent chooserIntent = Intent.createChooser(wall_intent,
-////                                                            "Set As");
-////                                                    chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-////                                                    chooserIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//                                                    result.success(true);
-////                                                    try {
-////                                                        getApplicationContext().startActivity(chooserIntent);
-////                                                    }catch (Exception e)
-////                                                    {
-////                                                        e.printStackTrace();
-////                                                    }
-//                                                } catch (IOException ex) {
-//                                                    ex.printStackTrace();
-//                                                }
-                                            }
-                                            @Override
-                                            public void onLoadCleared(@Nullable Drawable placeholder) {
-                                            }
-                                        });
+                                    Log.i("Arguments ", "configureFlutterEngine: " + url);
+                                    Glide.with(getActivity())
+                                            .asBitmap()
+                                            .load(url)
+                                            .into(new CustomTarget<Bitmap>() {
+                                                @Override
+                                                public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                                    Log.i("Arguments ", "configureFlutterEngine: " + "Ready 1");
+//                                                SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity());
+//                                                setWallPaperTask.execute(new Pair(resource,"1"));
+                                                    saveImage(resource);
+                                                }
 
-                            } else if (call.method.equals("set_home_wallpaper")){
+                                                @Override
+                                                public void onLoadCleared(@Nullable Drawable placeholder) {
+                                                }
+                                            });
 
-                                String url = call.argument("url"); // .argument returns the correct type
-                                android.util.Log.i("Arguments ", "configureFlutterEngine: "+url);
+                                    break;
+                                }
+                                case "set_home_wallpaper": {
+                                    METHOD = 2;
+                                    String url = call.argument("url"); // .argument returns the correct type
 
-                                Glide.with(getActivity())
-                                        .asBitmap()
-                                        .load(url)
-                                        .into(new CustomTarget<Bitmap>() {
-                                            @Override
-                                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                                                android.util.Log.i("Arguments ", "configureFlutterEngine: "+"Ready");
-                                                SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity());
-                                                setWallPaperTask.execute(new Pair  (resource,"2"));
-//                                                WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
-//                                                try {
-//                                                    wallpaperManager.setBitmap(resource,null,true,WallpaperManager.FLAG_SYSTEM);
-//                                                    result.success(true);
-//                                                } catch (IOException ex) {
-//                                                    ex.printStackTrace();
-//                                                }
-                                            }
-                                            @Override
-                                            public void onLoadCleared(@Nullable Drawable placeholder) {
-                                            }
-                                        });
+                                    Log.i("Arguments ", "configureFlutterEngine: " + url);
+                                    Glide.with(getActivity())
+                                            .asBitmap()
+                                            .load(url)
+                                            .into(new CustomTarget<Bitmap>() {
+                                                @Override
+                                                public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                                    Log.i("Arguments ", "configureFlutterEngine: " + "Ready");
+//                                                SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity());
+//                                                setWallPaperTask.execute(new Pair  (resource,"2"));
+                                                    saveImage(resource);
+                                                }
 
-                            }else if (call.method.equals("set_wallpaper")){
+                                                @Override
+                                                public void onLoadCleared(@Nullable Drawable placeholder) {
+                                                }
+                                            });
+                                    break;
+                                }
+                                case "set_wallpaper": {
+                                    METHOD = 3;
+                                    String url = call.argument("url"); // .argument returns the correct type
+                                    Log.i("Arguments ", "configureFlutterEngine: " + url);
+                                    Glide.with(getActivity())
+                                            .asBitmap()
+                                            .load(url)
+                                            .into(new CustomTarget<Bitmap>() {
+                                                @Override
+                                                public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                                    Log.i("Arguments ", "configureFlutterEngine: " + "Ready");
+//                                                SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity());
+//                                                setWallPaperTask.execute(new Pair(resource,"3"));
+                                                    saveImage(resource);
+                                                }
 
-                                String url = call.argument("url"); // .argument returns the correct type
-                                android.util.Log.i("Arguments ", "configureFlutterEngine: "+url);
+                                                @Override
+                                                public void onLoadCleared(@Nullable Drawable placeholder) {
+                                                }
+                                            });
 
-                                Glide.with(getActivity())
-                                        .asBitmap()
-                                        .load(url)
-                                        .into(new CustomTarget<Bitmap>() {
-                                            @Override
-                                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                                                android.util.Log.i("Arguments ", "configureFlutterEngine: "+"Ready");
-                                                SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity());
-                                                setWallPaperTask.execute(new Pair(resource,"3"));
-//                                                WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
-//                                                try {
-//                                                    wallpaperManager.setBitmap(resource);
-//                                                    result.success(true);
-//                                                } catch (IOException ex) {
-//                                                    ex.printStackTrace();
-//                                                }
-                                            }
-                                            @Override
-                                            public void onLoadCleared(@Nullable Drawable placeholder) {
-                                            }
-                                        });
-
+                                    break;
+                                }
                             }
                         }
                 );
     }
-}
 
-class SetWallPaperTask extends AsyncTask<Pair<Bitmap,String>,Boolean,Boolean>{
 
-    private final Context mContext;
-    public SetWallPaperTask(final Context context) {
-        mContext = context;
+    private void saveImage(Bitmap image) {
+        File pictureFile = getOutputMediaFile();
+        if (pictureFile == null) {
+            Log.d("Error",
+                    "Error creating media file, check storage permissions: ");// e.getMessage());
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d("TAG", "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d("TAG", "Error accessing file: " + e.getMessage());
+        }
+
+        try {
+            Uri uri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", pictureFile);
+            URI = uri;
+            performCrop(uri);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Create a File for saving an image or video
+     */
+    private File getOutputMediaFile() {
+
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + getApplicationContext().getPackageName()
+                + "/Files");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File mediaFile;
+        String mImageName = "MI_" + timeStamp + ".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        Log.i("  ", "getOutputMediaFile: " + mediaFile.getAbsolutePath());
+        Log.i("  ", "getOutputMediaFile: " + mediaFile.getPath());
+        return mediaFile;
+    }
+
+
+    private void performCrop(Uri uri) {
+        CropImage.activity(uri).setInitialCropWindowPaddingRatio(0.0f)
+                .start(this);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity(), URI);
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                    if (METHOD == 1)
+                        setWallPaperTask.execute(new Pair(bitmap, "1"));
+                    else if (METHOD == 2)
+                        setWallPaperTask.execute(new Pair(bitmap, "2"));
+                    else if (METHOD == 3)
+                        setWallPaperTask.execute(new Pair(bitmap, "3"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+}
+
+class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean> {
+
+    private final Context mContext;
+    private final Uri uri;
+
+    public SetWallPaperTask(final Context context, Uri URI) {
+        mContext = context;
+        uri = URI;
+    }
+
+    @SafeVarargs
     @Override
     protected final Boolean doInBackground(Pair<Bitmap, String>... pairs) {
         switch (pairs[0].second) {
@@ -159,6 +233,8 @@ class SetWallPaperTask extends AsyncTask<Pair<Bitmap,String>,Boolean,Boolean>{
                     ex.printStackTrace();
                     return false;
                 }
+                mContext.getContentResolver().delete(uri, null, null);
+                Log.i("File", "Deleted " + uri);
                 break;
             }
             case "2": {
@@ -171,6 +247,8 @@ class SetWallPaperTask extends AsyncTask<Pair<Bitmap,String>,Boolean,Boolean>{
                     ex.printStackTrace();
                     return false;
                 }
+                mContext.getContentResolver().delete(uri, null, null);
+                Log.i("File", "Deleted " + uri);
                 break;
             }
             case "3": {
@@ -181,6 +259,8 @@ class SetWallPaperTask extends AsyncTask<Pair<Bitmap,String>,Boolean,Boolean>{
                     ex.printStackTrace();
                     return false;
                 }
+                mContext.getContentResolver().delete(uri, null, null);
+                Log.i("File", "Deleted " + uri);
                 break;
             }
         }
