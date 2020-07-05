7e3dddbeeb0b100e94a70b05221dd1b49226da56
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-06 01:37:21 +0530

    working with demo images

--- lib/ui/pages/setupScreen.dart
+++ lib/ui/pages/setupScreen.dart
@@ -2,6 +2,7 @@ import 'package:Prism/routes/router.dart';
 import 'package:Prism/ui/widgets/home/bottomNavBar.dart';
 import 'package:Prism/ui/widgets/home/headingChipBar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SetupScreen extends StatelessWidget {
   SetupScreen({
@@ -13,6 +14,11 @@ class SetupScreen extends StatelessWidget {
     print(navStack);
     return true;
   }
+
+  final PageController controller = PageController(
+    viewportFraction: 0.78,
+    initialPage: 0,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +34,59 @@ class SetupScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
         body: BottomBar(
           child: SafeArea(
-            child: Align(
+            child: Stack(
               alignment: Alignment.topCenter,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.8,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/White.png'),
-                      fit: BoxFit.contain),
+              children: <Widget>[
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: PageView.builder(
+                    controller: controller,
+                    itemCount: 10,
+                    itemBuilder: (context, index) => Padding(
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.03),
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.67,
+                          height: MediaQuery.of(context).size.height * 0.72,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://firebasestorage.googleapis.com/v0/b/prism-wallpapers.appspot.com/o/image_picker6234277413502819198.jpg?alt=media&token=dbfb7a56-3b6e-4f8b-9fc0-5340d9ba6d60'),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                GestureDetector(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.83,
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/Black.png'),
+                          fit: BoxFit.fill),
+                    ),
+                  ),
+                  onPanUpdate: (details) {
+                    if (details.delta.dx < -10) {
+                      controller.animateToPage(controller.page.toInt() + 1,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.fastOutSlowIn);
+                      HapticFeedback.vibrate();
+                    } else if (details.delta.dx > 10) {
+                      controller.animateToPage(controller.page.toInt() - 1,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.fastOutSlowIn);
+                      HapticFeedback.vibrate();
+                    }
+                  },
+                ),
+              ],
             ),
           ),
         ),
