de21baddd783ef4095ac1905a32bbffb69b3305b
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-06 02:53:40 +0530

    Added desc, and text below setup

--- lib/ui/pages/setupScreen.dart
+++ lib/ui/pages/setupScreen.dart
@@ -4,6 +4,7 @@ import 'package:Prism/theme/jam_icons_icons.dart';
 import 'package:Prism/ui/widgets/animated/loader.dart';
 import 'package:Prism/ui/widgets/home/bottomNavBar.dart';
 import 'package:Prism/ui/widgets/home/headingChipBar.dart';
+import 'package:Prism/ui/widgets/home/inheritedScrollControllerProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:optimized_cached_image/widgets.dart';
@@ -51,134 +52,188 @@ class _SetupScreenState extends State<SetupScreen> {
         backgroundColor: Theme.of(context).primaryColor,
         body: BottomBar(
           child: SafeArea(
-            child: Stack(
-              alignment: Alignment.topCenter,
-              children: <Widget>[
-                FutureBuilder(
-                    future: future,
-                    builder: (context, snapshot) {
-                      if (snapshot == null) {
-                        print("snapshot null");
-                        return Loader();
-                      }
-                      if (snapshot.connectionState == ConnectionState.waiting ||
-                          snapshot.connectionState == ConnectionState.none) {
-                        print("snapshot none, waiting");
-                        return Loader();
-                      } else {
-                        return SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                          child: PageView.builder(
-                            controller: controller,
-                            itemCount: Provider.of<SetupProvider>(context,
-                                            listen: false)
-                                        .setups
-                                        .length ==
-                                    0
-                                ? 1
-                                : Provider.of<SetupProvider>(context,
-                                        listen: false)
-                                    .setups
-                                    .length,
-                            itemBuilder: (context, index) => Provider.of<
-                                                SetupProvider>(context,
-                                            listen: false)
-                                        .setups
-                                        .length ==
-                                    0
-                                ? Loader()
-                                : Padding(
-                                    padding: EdgeInsets.only(
-                                        top:
-                                            MediaQuery.of(context).size.height *
-                                                0.0299),
-                                    child: Align(
-                                      alignment: Alignment.topCenter,
-                                      child: OptimizedCacheImage(
-                                        imageUrl: Provider.of<SetupProvider>(
-                                                context,
-                                                listen: false)
-                                            .setups[index]['image'],
-                                        imageBuilder:
-                                            (context, imageProvider) =>
-                                                Container(
-                                          width: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              0.672,
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.72,
-                                          decoration: BoxDecoration(
-                                            image: DecorationImage(
-                                                image: imageProvider,
-                                                fit: BoxFit.fill),
-                                          ),
-                                        ),
-                                        placeholder: (context, url) =>
-                                            Container(
-                                          width: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              0.672,
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.72,
-                                          child: Center(
-                                            child: Loader(),
-                                          ),
-                                        ),
-                                        errorWidget: (context, url, error) =>
-                                            Container(
-                                          child: Center(
-                                            child: Icon(
-                                              JamIcons.close_circle_f,
-                                              color:
-                                                  Theme.of(context).accentColor,
-                                            ),
-                                          ),
-                                        ),
+            child: SetupPage(future: future, controller: controller),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SetupPage extends StatefulWidget {
+  const SetupPage({
+    Key key,
+    @required this.future,
+    @required this.controller,
+  }) : super(key: key);
+
+  final Future future;
+  final PageController controller;
+
+  @override
+  _SetupPageState createState() => _SetupPageState();
+}
+
+class _SetupPageState extends State<SetupPage> {
+  int pageNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    final ScrollController scrollController =
+        InheritedDataProvider.of(context).scrollController;
+    return SingleChildScrollView(
+      controller: scrollController,
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: <Widget>[
+          FutureBuilder(
+              future: widget.future,
+              builder: (context, snapshot) {
+                if (snapshot == null) {
+                  print("snapshot null");
+                  return Loader();
+                }
+                if (snapshot.connectionState == ConnectionState.waiting ||
+                    snapshot.connectionState == ConnectionState.none) {
+                  print("snapshot none, waiting");
+                  return Loader();
+                } else {
+                  return SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: PageView.builder(
+                      onPageChanged: (value) {
+                        setState(() {
+                          pageNumber = value;
+                        });
+                      },
+                      controller: widget.controller,
+                      itemCount: Provider.of<SetupProvider>(context,
+                                      listen: false)
+                                  .setups
+                                  .length ==
+                              0
+                          ? 1
+                          : Provider.of<SetupProvider>(context, listen: false)
+                              .setups
+                              .length,
+                      itemBuilder: (context, index) => Provider.of<
+                                      SetupProvider>(context, listen: false)
+                                  .setups
+                                  .length ==
+                              0
+                          ? Loader()
+                          : Padding(
+                              padding: EdgeInsets.only(
+                                  top: MediaQuery.of(context).size.height *
+                                      0.0299),
+                              child: Align(
+                                alignment: Alignment.topCenter,
+                                child: OptimizedCacheImage(
+                                  imageUrl: Provider.of<SetupProvider>(context,
+                                          listen: false)
+                                      .setups[index]['image'],
+                                  imageBuilder: (context, imageProvider) =>
+                                      Container(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.672,
+                                    height: MediaQuery.of(context).size.height *
+                                        0.72,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: imageProvider,
+                                          fit: BoxFit.fill),
+                                    ),
+                                  ),
+                                  placeholder: (context, url) => Container(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.672,
+                                    height: MediaQuery.of(context).size.height *
+                                        0.72,
+                                    child: Center(
+                                      child: Loader(),
+                                    ),
+                                  ),
+                                  errorWidget: (context, url, error) =>
+                                      Container(
+                                    child: Center(
+                                      child: Icon(
+                                        JamIcons.close_circle_f,
+                                        color: Theme.of(context).accentColor,
                                       ),
                                     ),
                                   ),
-                          ),
-                        );
-                      }
-                    }),
-                GestureDetector(
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    height: MediaQuery.of(context).size.height * 0.8,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image:
-                              Theme.of(context).accentColor == Color(0xFF2F2F2F)
-                                  ? AssetImage('assets/images/Black.png')
-                                  : AssetImage('assets/images/White.png'),
-                          fit: BoxFit.fill),
+                                ),
+                              ),
+                            ),
                     ),
+                  );
+                }
+              }),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height * 0.116,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    Provider.of<SetupProvider>(context, listen: false)
+                                .setups
+                                .length ==
+                            0
+                        ? ""
+                        : Provider.of<SetupProvider>(context, listen: false)
+                            .setups[pageNumber]['name'],
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline1
+                        .copyWith(fontSize: 30),
                   ),
-                  onPanUpdate: (details) {
-                    if (details.delta.dx < -10) {
-                      controller.animateToPage(controller.page.toInt() + 1,
-                          duration: Duration(milliseconds: 300),
-                          curve: Curves.fastOutSlowIn);
-                      HapticFeedback.vibrate();
-                    } else if (details.delta.dx > 10) {
-                      controller.animateToPage(controller.page.toInt() - 1,
-                          duration: Duration(milliseconds: 300),
-                          curve: Curves.fastOutSlowIn);
-                      HapticFeedback.vibrate();
-                    }
-                  },
-                ),
-              ],
+                  Text(
+                    Provider.of<SetupProvider>(context, listen: false)
+                                .setups
+                                .length ==
+                            0
+                        ? ""
+                        : Provider.of<SetupProvider>(context, listen: false)
+                            .setups[pageNumber]['desc'],
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
+          GestureDetector(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.83,
+              height: MediaQuery.of(context).size.height * 0.8,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: Theme.of(context).accentColor == Color(0xFF2F2F2F)
+                        ? AssetImage('assets/images/Black.png')
+                        : AssetImage('assets/images/White.png'),
+                    fit: BoxFit.fill),
+              ),
+            ),
+            onPanUpdate: (details) {
+              if (details.delta.dx < -10) {
+                widget.controller.animateToPage(
+                    widget.controller.page.toInt() + 1,
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.fastOutSlowIn);
+                HapticFeedback.vibrate();
+              } else if (details.delta.dx > 10) {
+                widget.controller.animateToPage(
+                    widget.controller.page.toInt() - 1,
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.fastOutSlowIn);
+                HapticFeedback.vibrate();
+              }
+            },
+          ),
+        ],
       ),
     );
   }
