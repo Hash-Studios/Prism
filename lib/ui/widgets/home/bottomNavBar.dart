6d11004b4f737706882255994fe3c38ea3e1ab45
Abhay Maurya <maurya.abhay30@gmail.com>
2020-07-16 19:21:58 +0530

    removed excess code from bottomnavbar

--- lib/ui/widgets/home/bottomNavBar.dart
+++ lib/ui/widgets/home/bottomNavBar.dart
@@ -391,22 +391,3 @@ class _BottomNavBarState extends State<BottomNavBar>
     );
   }
 }
-
-extension NavigatorStateExtension on NavigatorState {
-  void pushNamedIfNotCurrent(String routeName, {Object arguments}) {
-    if (!isCurrent(routeName)) {
-      pushNamed(routeName, arguments: arguments);
-    }
-  }
-
-  bool isCurrent(String routeName) {
-    bool isCurrent = false;
-    popUntil((route) {
-      if (route.settings.name == routeName) {
-        isCurrent = true;
-      }
-      return true;
-    });
-    return isCurrent;
-  }
-}
