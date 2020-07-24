e9359af267e840be5afab964ccae02146f02d553
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-07-25 01:48:46 +0530

    added secure flags

--- android/app/src/main/java/com/hash/Prism/Application.java
+++ android/app/src/main/java/com/hash/Prism/Application.java
@@ -1,6 +1,7 @@
 package com.hash.prism;
 
 import io.flutter.app.FlutterApplication;
+import android.view.WindowManager.LayoutParams;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -12,6 +13,7 @@ public class Application extends FlutterApplication implements PluginRegistrantC
   public void onCreate() {
     super.onCreate();
     FlutterFirebaseMessagingService.setPluginRegistrant(this);
+    getWindow().addFlags(LayoutParams.FLAG_SECURE);
   }
 
   @Override
