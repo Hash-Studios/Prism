8d014cc8adb4fcbaec580794e1e132346c747c22
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-08-10 22:27:11 +0530

    added categories list

---
+++ lib/data/categories/categories.dart
@@ -0,0 +1,124 @@
+import 'package:Prism/theme/jam_icons_icons.dart';
+
+final List categories = [
+  {
+    'name': 'Community',
+    'provider': "Prism",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Curated',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Popular',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Abstract',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Landscape',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Nature',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': '4K',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Art',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Pattern',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Minimal',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Anime',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Textures',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Technology',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Monochrome',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Code',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Space',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Cars',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Animals',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Skyscape',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Neon',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Architecture',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Sports',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Marvel',
+    'provider': "WallHaven",
+    'icon': JamIcons.pizza_slice,
+  },
+  {
+    'name': 'Music',
+    'provider': "Pexels",
+    'icon': JamIcons.pizza_slice,
+  },
+];
--- lib/global/categoryProvider.dart
+++ lib/global/categoryProvider.dart
@@ -1,132 +1,19 @@
+import 'package:Prism/data/categories/categories.dart';
 import 'package:Prism/data/prism/provider/prismWithoutProvider.dart' as Data;
 import 'package:Prism/data/wallhaven/provider/wallhavenWithoutProvider.dart'
     as WData;
 import 'package:Prism/global/customPopupMenu.dart';
-import 'package:Prism/theme/jam_icons_icons.dart';
 import 'package:flutter/cupertino.dart';
 
-final List choices = [
-  CustomPopupMenu(
-      title: 'Community',
-      // func: Data.getPrismWalls(),
-      provider: "Prism",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Curated',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Shuffle',
-      // func: WData.getData("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Abstract',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Landscape',
-      // func: WData.getLandscapeWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Nature',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: '4K',
-      // func: WData.get4KWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Art',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Pattern',
-      // func: WData.getPatternWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Minimal',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Anime',
-      // func: WData.getAnimeWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Textures',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Technology',
-      // func: WData.getTechnologyWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Monochrome',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Code',
-      // func: WData.getCodeWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Space',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Cars',
-      // func: WData.getCarsWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Animals',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Skyscape',
-      // func: WData.getSkyscapeWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Neon',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Architecture',
-      // func: WData.getArchitectureWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Sports',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Marvel',
-      // func: WData.getMarvelWalls("r"),
-      provider: "WallHaven",
-      icon: JamIcons.pizza_slice),
-  CustomPopupMenu(
-      title: 'Music',
-      // func: home(),
-      provider: "Pexels",
-      icon: JamIcons.pizza_slice),
-];
+final List choices = categories
+    .map(
+      (category) => CustomPopupMenu(
+        name: category['name'],
+        provider: category['provider'],
+        icon: category['icon'],
+      ),
+    )
+    .toList();
 
 class CategorySupplier extends ChangeNotifier {
   Future<List> wallpaperFutureRefresh = Data.getPrismWalls();
@@ -141,31 +28,31 @@ class CategorySupplier extends ChangeNotifier {
   }
 
   Future<List> changeWallpaperFuture(choice, String mode) {
-    this.wallpaperFutureRefresh = choice.title == choices[0].title
+    this.wallpaperFutureRefresh = choice.name == choices[0].name
         ? Data.getPrismWalls()
-        : choice.title == choices[2].title
+        : choice.name == choices[2].name
             ? WData.getData(mode)
-            : choice.title == choices[4].title
+            : choice.name == choices[4].name
                 ? WData.getLandscapeWalls(mode)
-                : choice.title == choices[6].title
+                : choice.name == choices[6].name
                     ? WData.get4KWalls(mode)
-                    : choice.title == choices[8].title
+                    : choice.name == choices[8].name
                         ? WData.getPatternWalls(mode)
-                        : choice.title == choices[10].title
+                        : choice.name == choices[10].name
                             ? WData.getAnimeWalls(mode)
-                            : choice.title == choices[12].title
+                            : choice.name == choices[12].name
                                 ? WData.getTechnologyWalls(mode)
-                                : choice.title == choices[14].title
+                                : choice.name == choices[14].name
                                     ? WData.getCodeWalls(mode)
-                                    : choice.title == choices[16].title
+                                    : choice.name == choices[16].name
                                         ? WData.getCarsWalls(mode)
-                                        : choice.title == choices[18].title
+                                        : choice.name == choices[18].name
                                             ? WData.getSkyscapeWalls(mode)
-                                            : choice.title == choices[20].title
+                                            : choice.name == choices[20].name
                                                 ? WData.getArchitectureWalls(
                                                     mode)
-                                                : choice.title ==
-                                                        choices[22].title
+                                                : choice.name ==
+                                                        choices[22].name
                                                     ? WData.getMarvelWalls(mode)
                                                     : WData.getData(mode);
     notifyListeners();
