569a7bc4348f6b3ba07ed6792064f3d1985f1305
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-08-15 18:27:05 +0530

    Revert "replaced glide with picasso"
    This reverts commit 4f9c80c1af77a5c293cc162b9d0570bcdc6b1e5e.

--- android/app/src/main/java/com/hash/Prism/MainActivity.java
+++ android/app/src/main/java/com/hash/Prism/MainActivity.java
@@ -1,10 +1,10 @@
 package com.hash.prism;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import android.annotation.SuppressLint;
 import android.content.Context;
 
+import com.bumptech.glide.request.target.CustomTarget;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
@@ -23,53 +23,52 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
 
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
-import com.squareup.picasso.Callback;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.transition.Transition;
 import java.io.*;
 import java.io.File;
 import java.io.IOException;
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "flutter.prism.set_wallpaper";
-    public static MethodChannel.Result res;
-    private Target target = new Target() {
-        @Override
-        public void onBitmapLoaded(Bitmap resource, Picasso.LoadedFrom from) {
-            android.util.Log.i("Arguments ", "configureFlutterEngine: " + "Ready");
-            SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity());
-            setWallPaperTask.execute(new Pair(resource, "1"));
-        }
-
-        @Override
-        public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-        }
-
-        @Override
-        public void onPrepareLoad(Drawable placeHolderDrawable) {
-        }
-    };
-
+    public static  MethodChannel.Result res;
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-                .setMethodCallHandler((call, result) -> {
-                    res = result;
-                    if (call.method.equals("set_wallpaper")) {
-                        String url = call.argument("url"); // .argument returns the correct type
-                        android.util.Log.i("Arguments ", "configureFlutterEngine: " + url);
-                        Picasso.get().load(url).into(target);
+                .setMethodCallHandler(
+                        (call, result) -> {
 
-                    }
-                });
+                            res = result;
+                            if (call.method.equals("set_wallpaper")){
+
+                                String url = call.argument("url"); // .argument returns the correct type
+                                android.util.Log.i("Arguments ", "configureFlutterEngine: "+url);
+
+                                Glide.with(getActivity())
+                                        .asBitmap()
+                                        .load(url)
+                                        .into(new CustomTarget<Bitmap>() {
+                                            @Override
+                                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                                android.util.Log.i("Arguments ", "configureFlutterEngine: "+"Ready");
+                                                SetWallPaperTask setWallPaperTask = new SetWallPaperTask(getActivity());
+                                                setWallPaperTask.execute(new Pair(resource,"1"));
+                                            }
+                                            @Override
+                                            public void onLoadCleared(@Nullable Drawable placeholder) {
+                                            }
+                                        });
+
+                            }
+                        }
+                );
     }
 }
 
-class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean> {
+class SetWallPaperTask extends AsyncTask<Pair<Bitmap,String>,Boolean,Boolean>{
 
     private final Context mContext;
-
     public SetWallPaperTask(final Context context) {
         mContext = context;
     }
@@ -106,34 +105,33 @@ class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean>
 
     public static Uri getImageContentUri(Context context, String absPath) {
 
-        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                new String[] { MediaStore.Images.Media._ID }, MediaStore.Images.Media.DATA + "=? ",
-                new String[] { absPath }, null);
+    Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            new String[] { MediaStore.Images.Media._ID }, MediaStore.Images.Media.DATA + "=? ",
+            new String[] { absPath }, null);
 
-        if (cursor != null && cursor.moveToFirst()) {
-            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
-            return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, Integer.toString(id));
+    if (cursor != null && cursor.moveToFirst()) {
+      int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+      return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, Integer.toString(id));
 
-        } else if (!absPath.isEmpty()) {
-            ContentValues values = new ContentValues();
-            values.put(MediaStore.Images.Media.DATA, absPath);
-            return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        } else {
-            return null;
-        }
+    } else if (!absPath.isEmpty()) {
+      ContentValues values = new ContentValues();
+      values.put(MediaStore.Images.Media.DATA, absPath);
+      return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    } else {
+      return null;
+    }
+  }
+  public Uri getImageUri(Context inContext, Bitmap inImage) {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+    String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+    return Uri.parse(path);
     }
 
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    public String getRealPathFromURI(Uri uri) {
-        Cursor cursor = mContext.getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
+  public String getRealPathFromURI(Uri uri) {
+    Cursor cursor = mContext.getContentResolver().query(uri, null, null, null, null); 
+    cursor.moveToFirst(); 
+    int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA); 
+    return cursor.getString(idx); 
     }
 }
