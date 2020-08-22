5a739056d5b09f21ba080ee62b053132882a279c
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-08-22 19:05:17 +0530

    added tap and hold on set wallpaper

--- lib/ui/widgets/menuButton/setWallpaperButton.dart
+++ lib/ui/widgets/menuButton/setWallpaperButton.dart
@@ -64,6 +64,129 @@ class _SetWallpaperButtonState extends State<SetWallpaperButton> {
     }
   }
 
+  void _setBothWallPaper() async {
+    bool result;
+    try {
+      if (widget.url.contains("com.hash.prism")) {
+        result = await platform
+            .invokeMethod("set_both_wallpaper_file", <String, dynamic>{
+          'url': widget.url,
+        });
+      } else if (widget.url.contains("/0/")) {
+        result = await platform
+            .invokeMethod("set_both_wallpaper_file", <String, dynamic>{
+          'url': "/" + widget.url.replaceAll("/0//", "/0/"),
+        });
+      } else {
+        result =
+            await platform.invokeMethod("set_both_wallpaper", <String, dynamic>{
+          'url': widget.url,
+        });
+      }
+      if (result) {
+        print("Success");
+        analytics.logEvent(
+            name: 'set_wall',
+            parameters: {'type': 'Both', 'result': 'Success'});
+        toasts.codeSend("Wallpaper set successfully!");
+      } else {
+        print("Failed");
+        toasts.error("Something went wrong!");
+      }
+      if (this.mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      analytics.logEvent(
+          name: 'set_wall', parameters: {'type': 'Both', 'result': 'Failure'});
+      print(e);
+    }
+  }
+
+  void _setLockWallPaper() async {
+    bool result;
+    try {
+      if (widget.url.contains("com.hash.prism")) {
+        result = await platform
+            .invokeMethod("set_lock_wallpaper_file", <String, dynamic>{
+          'url': widget.url,
+        });
+      } else if (widget.url.contains("/0/")) {
+        result = await platform
+            .invokeMethod("set_lock_wallpaper_file", <String, dynamic>{
+          'url': "/" + widget.url.replaceAll("/0//", "/0/"),
+        });
+      } else {
+        result =
+            await platform.invokeMethod("set_lock_wallpaper", <String, dynamic>{
+          'url': widget.url,
+        });
+      }
+      if (result) {
+        print("Success");
+        analytics.logEvent(
+            name: 'set_wall',
+            parameters: {'type': 'Lock', 'result': 'Success'});
+        toasts.codeSend("Wallpaper set successfully!");
+      } else {
+        print("Failed");
+        toasts.error("Something went wrong!");
+      }
+      if (this.mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      print(e);
+      analytics.logEvent(
+          name: 'set_wall', parameters: {'type': 'Lock', 'result': 'Failure'});
+    }
+  }
+
+  void _setHomeWallPaper() async {
+    bool result;
+    try {
+      if (widget.url.contains("com.hash.prism")) {
+        result = await platform
+            .invokeMethod("set_home_wallpaper_file", <String, dynamic>{
+          'url': widget.url,
+        });
+      } else if (widget.url.contains("/0/")) {
+        result = await platform
+            .invokeMethod("set_home_wallpaper_file", <String, dynamic>{
+          'url': "/" + widget.url.replaceAll("/0//", "/0/"),
+        });
+      } else {
+        result =
+            await platform.invokeMethod("set_home_wallpaper", <String, dynamic>{
+          'url': widget.url,
+        });
+      }
+      if (result) {
+        print("Success");
+        analytics.logEvent(
+            name: 'set_wall',
+            parameters: {'type': 'Home', 'result': 'Success'});
+        toasts.codeSend("Wallpaper set successfully!");
+      } else {
+        print("Failed");
+        toasts.error("Something went wrong!");
+      }
+      if (this.mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      print(e);
+      analytics.logEvent(
+          name: 'set_wall', parameters: {'type': 'Home', 'result': 'Failure'});
+    }
+  }
+
   void showPremiumPopUp(Function func) {
     if (!main.prefs.get("premium")) {
       toasts.codeSend("Variants are a premium feature.");
@@ -91,11 +214,111 @@ class _SetWallpaperButtonState extends State<SetWallpaperButton> {
     }
   }
 
+  void onTapPaint() async {
+    showDialog(
+      context: context,
+      child: AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        content: Container(
+          height: 200,
+          width: 250,
+          child: Center(
+            child: ListView.builder(
+                itemCount: 3,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Icon(
+                      index == 0
+                          ? JamIcons.phone
+                          : index == 1 ? JamIcons.key : JamIcons.picture,
+                      color: Theme.of(context).accentColor,
+                    ),
+                    title: Text(
+                      index == 0
+                          ? "Home Screen"
+                          : index == 1 ? "Lock Screen" : "Both",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    onTap: index == 0
+                        ? () async {
+                            HapticFeedback.vibrate();
+                            Navigator.of(context).pop();
+                            if (widget.colorChanged) {
+                              showPremiumPopUp(() async {
+                                setState(() {
+                                  isLoading = true;
+                                });
+                                Future.delayed(Duration(seconds: 1))
+                                    .then((value) => _setHomeWallPaper());
+                              });
+                            } else {
+                              setState(() {
+                                isLoading = true;
+                              });
+                              Future.delayed(Duration(seconds: 1))
+                                  .then((value) => _setHomeWallPaper());
+                            }
+                          }
+                        : index == 1
+                            ? () async {
+                                HapticFeedback.vibrate();
+                                Navigator.of(context).pop();
+                                if (widget.colorChanged) {
+                                  showPremiumPopUp(() async {
+                                    setState(() {
+                                      isLoading = true;
+                                    });
+                                    Future.delayed(Duration(seconds: 1))
+                                        .then((value) => _setLockWallPaper());
+                                  });
+                                } else {
+                                  setState(() {
+                                    isLoading = true;
+                                  });
+                                  Future.delayed(Duration(seconds: 1))
+                                      .then((value) => _setLockWallPaper());
+                                }
+                              }
+                            : () async {
+                                HapticFeedback.vibrate();
+                                Navigator.of(context).pop();
+                                if (widget.colorChanged) {
+                                  showPremiumPopUp(() async {
+                                    setState(() {
+                                      isLoading = true;
+                                    });
+                                    Future.delayed(Duration(seconds: 1))
+                                        .then((value) => _setBothWallPaper());
+                                  });
+                                } else {
+                                  setState(() {
+                                    isLoading = true;
+                                  });
+                                  Future.delayed(Duration(seconds: 1))
+                                      .then((value) => _setBothWallPaper());
+                                }
+                              },
+                  );
+                }),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
+      onLongPress: () {
         onPaint();
+      },
+      onTap: () {
+        onTapPaint();
       },
       child: Stack(
         children: [
