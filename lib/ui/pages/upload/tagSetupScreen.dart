0ec67445aa44c3e51a91891e56fb3f1ca0fa165c
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-10-28 19:07:24 +0530

    added basic tags

--- lib/ui/pages/upload/tagSetupScreen.dart
+++ lib/ui/pages/upload/tagSetupScreen.dart
@@ -1,19 +1,13 @@
 import 'dart:io';
-import 'dart:math';
-import 'package:Prism/analytics/analytics_service.dart';
-import 'package:Prism/theme/jam_icons_icons.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
-import 'package:path/path.dart' as Path;
 import 'package:Prism/routes/router.dart';
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
-import 'package:Prism/data/upload/wallpaper/wallfirestore.dart' as WallStore;
-import 'package:Prism/theme/toasts.dart' as toasts;
 import 'package:Prism/main.dart' as main;
 import 'package:Prism/theme/config.dart' as config;
+
+List<Widget> tags = [];
 
 class TagSetupScreen extends StatefulWidget {
   final List arguments;
@@ -23,92 +17,41 @@ class TagSetupScreen extends StatefulWidget {
 }
 
 class _TagSetupScreenState extends State<TagSetupScreen> {
-  bool isUploading;
-  bool isProcessing;
   File image;
-  final ScrollController _scrollController = ScrollController();
-  String imageURL;
-  TextEditingController setupName = TextEditingController();
-  TextEditingController setupDesc = TextEditingController();
-  TextEditingController iconName = TextEditingController();
-  TextEditingController iconURL = TextEditingController();
-  TextEditingController widgetName = TextEditingController();
-  TextEditingController widgetURL = TextEditingController();
-  String id;
-  String tempid;
-  TextEditingController wallpaperUrl = TextEditingController();
-  String wallpaperProvider;
-  String wallpaperThumb;
-  bool review;
-  List<int> imageBytes;
+  int heightOfImage;
+  int widthOfImage;
+  int displayHeightOfImage;
+  int displayWidthOfImage;
+  double scale;
+
   @override
   void initState() {
     super.initState();
+    tags = [];
     image = widget.arguments[0] as File;
-    isUploading = false;
-    isProcessing = true;
-    randomId();
-    wallpaperProvider = "Prism";
-    wallpaperThumb = "";
-    review = false;
-    processImage();
-  }
-
-  void randomId() {
-    tempid = "";
-    final alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
-    final r = Random();
-    final choice = r.nextInt(6);
-    for (var i = 0; i < 6; i++) {
-      if (choice == i) {
-        final ran = r.nextInt(10);
-        tempid = tempid + ran.toString();
-      } else {
-        final ran = r.nextInt(26);
-        tempid = tempid + alp[ran].toString();
-      }
-    }
-    setState(() {
-      id = tempid;
-    });
-    debugPrint(id);
-  }
-
-  Future processImage() async {
-    imageBytes = await image.readAsBytes();
-    uploadFile();
-  }
-
-  Future uploadFile() async {
-    setState(() {
-      isUploading = true;
-      isProcessing = false;
-    });
-    try {
-      final StorageReference storageReference =
-          FirebaseStorage.instance.ref().child(Path.basename(image.path));
-      final StorageUploadTask uploadTask = storageReference.putFile(image);
-      await uploadTask.onComplete;
-      debugPrint('File Uploaded');
-      storageReference.getDownloadURL().then((fileURL) {
-        setState(() {
-          imageURL = fileURL.toString();
-          isUploading = false;
-        });
-      });
-    } catch (e) {
-      debugPrint(e.toString());
-      Navigator.pop(context);
-      navStack.removeLast();
-      debugPrint(navStack.toString());
-      toasts.error("Some uploading issue, please try again.");
-    }
+    displayHeightOfImage = widget.arguments[1].round() as int;
+    _imageProcess();
   }
 
   Future<bool> onWillPop() async {
     if (navStack.length > 1) navStack.removeLast();
     debugPrint(navStack.toString());
     return true;
+  }
+
+  Future<void> _imageProcess() async {
+    final imgList = image.readAsBytesSync();
+    final decodedImage = await decodeImageFromList(imgList);
+    heightOfImage = decodedImage.height;
+    widthOfImage = decodedImage.width;
+    scale = displayHeightOfImage / heightOfImage;
+    displayWidthOfImage = (scale * widthOfImage).round();
+
+    debugPrint(decodedImage.width.toString());
+    debugPrint(decodedImage.height.toString());
+    debugPrint(displayWidthOfImage.toString());
+    debugPrint(displayHeightOfImage.toString());
+    debugPrint(scale.toString());
   }
 
   @override
@@ -119,203 +62,125 @@ class _TagSetupScreenState extends State<TagSetupScreen> {
         backgroundColor: Theme.of(context).primaryColor,
         appBar: AppBar(
           title: Text(
-            "Upload Setup",
+            "Tag Widgets",
             style: TextStyle(color: Theme.of(context).accentColor),
           ),
           actions: [
             TextButton(
-              onPressed: !isProcessing && !isUploading
-                  ? () async {
-                      if (setupName.text == "" ||
-                          setupDesc.text == "" ||
-                          wallpaperUrl.text == "" ||
-                          iconName.text == "" ||
-                          iconURL.text == "") {
-                        toasts.error("Please fill all required fields!");
-                      } else {
-                        navStack.removeLast();
-                        debugPrint(navStack.toString());
-                        Navigator.pop(context);
-                        analytics.logEvent(
-                            name: 'upload_setup',
-                            parameters: {'id': id, 'link': imageURL});
-                        WallStore.createSetup(
-                            id,
-                            imageURL,
-                            wallpaperProvider,
-                            wallpaperThumb,
-                            wallpaperUrl.text,
-                            iconName.text,
-                            iconURL.text,
-                            widgetName.text,
-                            widgetURL.text,
-                            setupName.text,
-                            setupDesc.text,
-                            review);
-                      }
-                    }
-                  : null,
+              onPressed: () {},
               child: Text(
-                "Post",
+                "Tag",
                 style: TextStyle(
-                  color: !isProcessing && !isUploading
-                      ? config.Colors().mainAccentColor(1)
-                      : Theme.of(context).hintColor,
+                  color: config.Colors().mainAccentColor(1),
+                  // color: !isProcessing && !isUploading
+                  //     ? config.Colors().mainAccentColor(1)
+                  //     : Theme.of(context).hintColor,
                   fontWeight: FontWeight.normal,
                 ),
               ),
             ),
           ],
         ),
-        body: ListView(children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: CircleAvatar(
-                  backgroundColor: config.Colors().mainAccentColor(1),
-                  radius: 20,
-                  child: main.prefs.get("googleimage") == null
-                      ? Container()
-                      : Image.network(main.prefs.get("googleimage").toString()),
+        body: Column(children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.7,
+            child: Stack(
+              children: [
+                GestureDetector(
+                  onTapUp: (details) {
+                    debugPrint(details.globalPosition.dx.toString());
+                    debugPrint(details.globalPosition.dy.toString());
+                    setState(() {
+                      tags.add(Tag(
+                          x: details.globalPosition.dx - 80,
+                          y: details.globalPosition.dy - 90));
+                    });
+                  },
+                  onTap: () async {},
+                  child: Image.file(image, fit: BoxFit.contain),
+                ),
+                for (var tag in tags) tag
+              ],
+            ),
+          ),
+          const Divider(height: 1),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Tap the setup to add widgets and icon packs.\nDrag to move or long press the tag to delete.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 11,
+                    fontWeight: FontWeight.w100,
+                    color: Theme.of(context).accentColor.withOpacity(0.6),
+                  ),
                 ),
               ),
-              const Spacer(),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.4,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(height: 20),
-                    TextField(
-                      controller: setupName,
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                        color: Theme.of(context).accentColor,
-                      ),
-                      decoration: InputDecoration(
-                        labelText: "Write a Name...",
-                        hintText: "Write a Name...",
-                        hintStyle: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          color: Theme.of(context).accentColor,
-                        ),
-                        labelStyle: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          color: Theme.of(context).accentColor,
-                        ),
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                        border: InputBorder.none,
-                      ),
-                    ),
-                    TextField(
-                      maxLines: 2,
-                      controller: setupDesc,
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                        color: Theme.of(context).accentColor,
-                      ),
-                      decoration: InputDecoration(
-                        labelText: "Write a description...",
-                        hintText: "Write a description...",
-                        hintStyle: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          color: Theme.of(context).accentColor,
-                        ),
-                        labelStyle: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          color: Theme.of(context).accentColor,
-                        ),
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const Spacer(flex: 10),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                          builder: (context) => PhotoView(
-                                onTapUp: (context, details, controller) {
-                                  Navigator.pop(context);
-                                },
-                                imageProvider: FileImage(image),
-                              ),
-                          fullscreenDialog: true));
-                },
-                child: Stack(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(20.0),
-                      height: 200,
-                      width: 120,
-                      child: Image.file(image, fit: BoxFit.contain),
-                    ),
-                    if (isUploading || isProcessing)
-                      Container(
-                        padding: const EdgeInsets.all(20.0),
-                        height: 200,
-                        width: 120,
-                        child: Center(
-                          child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                                config.Colors().mainAccentColor(1)),
-                          ),
-                        ),
-                      )
-                    else
-                      Container(),
-                  ],
-                ),
-              )
-            ],
-          ),
-          ListTile(
-            title: Text(
-              "Tag widgets, icon packs",
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
-                color: Theme.of(context).accentColor,
-              ),
             ),
-            onTap: () {},
           ),
-          ListTile(
-            title: Text(
-              "Add wallpaper",
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
-                color: Theme.of(context).accentColor,
-              ),
-            ),
-            onTap: () {},
-          ),
-          ListTile(
-            title: Text(
-              main.prefs.get('premium') == true
-                  ? "Note - We have a strong review policy, and submitting irrelevant images & info will lead to ban. Your photo will be visible in the profile/community section."
-                  : "Note - We have a strong review policy, and submitting irrelevant images & info will lead to ban. We take about 24 hours to review the submissions, and after a successful review, your setup will be visible in the setups section.",
-              style: TextStyle(
-                fontSize: 11,
-                fontWeight: FontWeight.w100,
-                color: Theme.of(context).accentColor.withOpacity(0.6),
-              ),
-            ),
-          )
         ]),
+      ),
+    );
+  }
+}
+
+class Tag extends StatefulWidget {
+  final double x;
+  final double y;
+  const Tag({@required this.x, @required this.y});
+  @override
+  _TagState createState() => _TagState();
+}
+
+class _TagState extends State<Tag> {
+  double xPosition = 0;
+  double yPosition = 0;
+  bool visibility = true;
+  Color color;
+  String link;
+  String name;
+
+  @override
+  void initState() {
+    color = Colors.black87;
+    super.initState();
+    setState(() {
+      xPosition = widget.x;
+      yPosition = widget.y;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: yPosition,
+      left: xPosition,
+      child: GestureDetector(
+        onLongPress: () {
+          setState(() {
+            visibility = false;
+            xPosition = 10000;
+            yPosition = 10000;
+            color = Colors.transparent;
+          });
+        },
+        onPanUpdate: (tapInfo) {
+          setState(() {
+            xPosition += tapInfo.delta.dx;
+            yPosition += tapInfo.delta.dy;
+          });
+        },
+        child: Container(
+          width: 70,
+          height: 30,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
       ),
     );
   }
