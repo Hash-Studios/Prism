09cf0b3f7bcefa4bd700a4a7415fb9ab7ecf1acb
Abhay Maurya <maurya.abhay30@gmail.com>
2020-11-02 15:36:14 +0530

    minor fixes in main

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:Prism/theme/theme.dart';
 import 'package:flutter/services.dart';
 import 'package:Prism/theme/config.dart' as config;
 
@@ -32,6 +31,8 @@ String currentThemeID;
 bool hqThumbs;
 bool optimisedWallpapers;
 void main() {
+  //! Uncomment next line before release
+  // debugPrint = (String message, {int wrapWidth}) {};
   WidgetsFlutterBinding.ensureInitialized();
   InAppPurchaseConnection.enablePendingPurchases();
   GestureBinding.instance.resamplingEnabled = true;
@@ -109,30 +110,12 @@ class _MyAppState extends State<MyApp> {
   Future<void> fetchModes() async {
     try {
       modes = await FlutterDisplayMode.supported;
-      modes.forEach(print);
-
-      /// On OnePlus 7 Pro:
-      /// #1 1080x2340 @ 60Hz
-      /// #2 1080x2340 @ 90Hz
-      /// #3 1440x3120 @ 90Hz
-      /// #4 1440x3120 @ 60Hz
-      /// On OnePlus 8 Pro:
-      /// #1 1080x2376 @ 60Hz
-      /// #2 1440x3168 @ 120Hz
-      /// #3 1440x3168 @ 60Hz
-      /// #4 1080x2376 @ 120Hz
+      // modes.forEach(print);
     } on PlatformException catch (e) {
-      print(e);
-
-      /// e.code =>
-      /// noAPI - No API support. Only Marshmallow and above.
-      /// noActivity - Activity is not available. Probably app is in background
+      debugPrint(e.toString());
     }
     selected =
         modes.firstWhere((DisplayMode m) => m.selected, orElse: () => null);
-    // if (mounted) {
-    //   setState(() {});
-    // }
   }
 
   Future<DisplayMode> getCurrentMode() async {
@@ -141,7 +124,7 @@ class _MyAppState extends State<MyApp> {
 
   Future<void> setCurrentMode(int index) async {
     await fetchModes();
-    if (modes.length > index - 1) {
+    if (modes.length > index) {
       await FlutterDisplayMode.setMode(modes[index]);
     }
     selected =
