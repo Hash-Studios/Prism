8573a147f3c38d76b3523de7152f51ac2df3e6ca
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-11-18 20:46:11 +0530

    fixed banner setup left

--- lib/ui/widgets/home/collections/collectionsGrid.dart
+++ lib/ui/widgets/home/collections/collectionsGrid.dart
@@ -461,7 +461,7 @@ class PremiumBannerSetupOld extends StatelessWidget {
               Positioned(
                 top: MediaQuery.of(context).size.height * 0.65 - 33,
                 left:
-                    MediaQuery.of(context).size.height * 0.65 * (9 / 19.5) - 17,
+                    MediaQuery.of(context).size.height * 0.65 * (9 / 19.5) - 25,
                 child: Container(
                   decoration: const BoxDecoration(
                       color: Color(0xFFFFB800),
