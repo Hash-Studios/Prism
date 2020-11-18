039f7b7db6f53150d5b449ff567f1ab264b03560
Abhay Maurya <maurya.abhay30@gmail.com>
2020-11-18 20:32:16 +0530

    updated version to 2.6.1+56

--- lib/ui/widgets/popup/changelogPopUp.dart
+++ lib/ui/widgets/popup/changelogPopUp.dart
@@ -40,6 +40,40 @@ void showChangelog(BuildContext context, Function func) {
               child: SingleChildScrollView(
                 controller: controller,
                 child: Column(mainAxisSize: MainAxisSize.min, children: const [
+                  ChangeVersion(number: 'v2.6.1'),
+                  Change(
+                      icon: JamIcons.download,
+                      text: "Fixed wallpaper downloading in Android 11."),
+                  Change(
+                      icon: JamIcons.wrench,
+                      text: "Redesigned setups' info panel!"),
+                  Change(
+                      icon: JamIcons.instant_picture,
+                      text: "Now see your uploaded setups in your profile!"),
+                  Change(
+                      icon: JamIcons.heart,
+                      text: "Now see your favourite setups in your profile!"),
+                  Change(
+                      icon: JamIcons.tools, text: "Added more new animations!"),
+                  Change(
+                      icon: JamIcons.settings_alt,
+                      text: "Turned wallpaper optimisation off by default."),
+                  Change(icon: JamIcons.egg, text: "Added some easter eggs."),
+                  Change(
+                      icon: JamIcons.crop,
+                      text: "Added new crop ratios in wallpaper upload."),
+                  Change(
+                      icon: JamIcons.phone,
+                      text: "Added support for phones with smaller screens."),
+                  Change(
+                      icon: JamIcons.user_circle,
+                      text: "Redesigned Profile Page."),
+                  Change(
+                      icon: JamIcons.bug,
+                      text: "Minor bug fixes and improvements."),
+                  SizedBox(
+                    height: 15,
+                  ),
                   ChangeVersion(number: 'v2.6.0'),
                   Change(
                       icon: JamIcons.instant_picture,
