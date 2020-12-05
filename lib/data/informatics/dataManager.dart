2831167f2704e06e3808752baaea0ba10add2ad2
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-12-05 20:17:55 +0530

    added views & other fns for setups

--- lib/data/informatics/dataManager.dart
+++ lib/data/informatics/dataManager.dart
@@ -91,3 +91,77 @@ Future<void> updateFavorites(String id) async {
       stringToBase64.encode(json.encode(jsonMap)),
       repoContents.file.sha);
 }
+
+Future<void> updateViewsSetup(String id) async {
+  Codec<String, String> stringToBase64 = utf8.fuse(base64);
+  final github = GitHub(auth: Authentication.withToken(token));
+  RepositoryContents repoContents;
+  Map jsonMap;
+  await github.repositories
+      .getContents(RepositorySlug(gitUserName, repoName3), jsonFile)
+      .then((value) => repoContents = value);
+  jsonMap = json.decode(
+          stringToBase64.decode(repoContents.file.content.replaceAll("\n", "")))
+      as Map;
+  jsonMap["setups"][id] = {
+    "views": jsonMap["setups"][id] != null
+        ? (int.parse(jsonMap["setups"][id]["views"].toString()) + 1).toString()
+        : "1"
+  };
+  await github.repositories.updateFile(
+      RepositorySlug(gitUserName, repoName3),
+      jsonFile,
+      "Updated views for $id",
+      stringToBase64.encode(json.encode(jsonMap)),
+      repoContents.file.sha);
+}
+
+Future<void> updateDownloadsSetup(String id) async {
+  Codec<String, String> stringToBase64 = utf8.fuse(base64);
+  final github = GitHub(auth: Authentication.withToken(token));
+  RepositoryContents repoContents;
+  Map jsonMap;
+  await github.repositories
+      .getContents(RepositorySlug(gitUserName, repoName3), jsonFile)
+      .then((value) => repoContents = value);
+  jsonMap = json.decode(
+          stringToBase64.decode(repoContents.file.content.replaceAll("\n", "")))
+      as Map;
+  jsonMap["setups"][id] = {
+    "downloads": jsonMap["setups"][id] != null
+        ? (int.parse(jsonMap["setups"][id]["downloads"].toString()) + 1)
+            .toString()
+        : "1"
+  };
+  await github.repositories.updateFile(
+      RepositorySlug(gitUserName, repoName3),
+      jsonFile,
+      "Updated downloads for $id",
+      stringToBase64.encode(json.encode(jsonMap)),
+      repoContents.file.sha);
+}
+
+Future<void> updateFavsSetup(String id) async {
+  Codec<String, String> stringToBase64 = utf8.fuse(base64);
+  final github = GitHub(auth: Authentication.withToken(token));
+  RepositoryContents repoContents;
+  Map jsonMap;
+  await github.repositories
+      .getContents(RepositorySlug(gitUserName, repoName3), jsonFile)
+      .then((value) => repoContents = value);
+  jsonMap = json.decode(
+          stringToBase64.decode(repoContents.file.content.replaceAll("\n", "")))
+      as Map;
+  jsonMap["setups"][id] = {
+    "favorites": jsonMap["setups"][id] != null
+        ? (int.parse(jsonMap["setups"][id]["favorites"].toString()) + 1)
+            .toString()
+        : "1"
+  };
+  await github.repositories.updateFile(
+      RepositorySlug(gitUserName, repoName3),
+      jsonFile,
+      "Updated favorites for $id",
+      stringToBase64.encode(json.encode(jsonMap)),
+      repoContents.file.sha);
+}
