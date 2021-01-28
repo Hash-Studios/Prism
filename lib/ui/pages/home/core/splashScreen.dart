b77ad08f52f6e9fe6967ae334dca34c61f48bf09
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2021-01-28 14:19:58 +0530

    fix #231

--- lib/ui/pages/home/core/splashScreen.dart
+++ lib/ui/pages/home/core/splashScreen.dart
@@ -16,62 +16,66 @@ RemoteConfig remoteConfig;
 class SplashWidget extends StatelessWidget {
   bool notchChecked = false;
   Future<void> rcInit() async {
-    remoteConfig = await RemoteConfig.instance;
-    await remoteConfig.setConfigSettings(RemoteConfigSettings());
-    await remoteConfig.setDefaults(<String, dynamic>{
-      'topImageLink':
-          'https://firebasestorage.googleapis.com/v0/b/prism-wallpapers.appspot.com/o/Replacement%20Thumbnails%2Fpost%20bg.png?alt=media&token=d708b5e3-a7ee-421b-beae-3b10946678c4',
-      'bannerText': "Join our Telegram",
-      'bannerTextOn': globals.bannerTextOn,
-      'bannerURL': "https://t.me/PrismWallpapers",
-      'latestCategories': categories.toString(),
-      'currentVersion': globals.currentAppVersion.toString(),
-      'obsoleteVersion': globals.obsoleteAppVersion.toString(),
-      'versionDesc':
-          "Prism Premium is here, for the personalisaton lords!^*^Setups are here! Change the way of personalisation.^*^Favourites moved to profile.",
-      'topTitleText':
-          '["TOP-RATED","BEST OF COMMUNITY","FAN-FAVOURITE","TRENDING",]',
-      'premiumCollections': '["space","landscapes","mesh gradients",]',
-      'verifiedUsers':
-          '["akshaymaurya3006@gmail.com","maurya.abhay30@gmail.com",]'
-    });
-    debugPrint("Started Fetching Values from rc");
-    await remoteConfig.fetch(expiration: const Duration(hours: 6));
-    await remoteConfig.activateFetched();
-    debugPrint("Fetched Values from rc");
-    globals.topImageLink = remoteConfig.getString('topImageLink');
-    globals.bannerText = remoteConfig.getString('bannerText');
-    globals.bannerTextOn = remoteConfig.getString('bannerTextOn');
-    globals.bannerURL = remoteConfig.getString('bannerURL');
-    globals.obsoleteAppVersion = remoteConfig.getString('obsoleteVersion');
-    var verifiedU = remoteConfig.getString('verifiedUsers');
-    verifiedU = verifiedU.replaceAll('"', '');
-    verifiedU = verifiedU.replaceAll("[", "");
-    verifiedU = verifiedU.replaceAll(",]", "");
-    globals.verifiedUsers = verifiedU.split(",");
-    var premiumC = remoteConfig.getString('premiumCollections');
-    premiumC = premiumC.replaceAll('"', '');
-    premiumC = premiumC.replaceAll("[", "");
-    premiumC = premiumC.replaceAll(",]", "");
-    globals.premiumCollections = premiumC.split(",");
-    var text = remoteConfig.getString('topTitleText');
-    text = text.replaceAll('"', '');
-    text = text.replaceAll("[", "");
-    text = text.replaceAll(",]", "");
-    globals.topTitleText = text.split(",");
-    globals.topTitleText.shuffle();
-    final cList = [];
-    var tempVar = remoteConfig
-        .getString('latestCategories')
-        .replaceAll('[', "")
-        .replaceAll(']', "")
-        .split("},");
-    tempVar = tempVar.sublist(0, tempVar.length - 1);
-    tempVar.forEach((element) {
-      cList.add(element.split('"name": "')[1].split('",')[0].toString());
-      categories[tempVar.indexOf(element)] = json.decode("$element}");
-    });
-    debugPrint(cList.toString());
+    try {
+      remoteConfig = await RemoteConfig.instance;
+      await remoteConfig.setConfigSettings(RemoteConfigSettings());
+      await remoteConfig.setDefaults(<String, dynamic>{
+        'topImageLink':
+            'https://firebasestorage.googleapis.com/v0/b/prism-wallpapers.appspot.com/o/Replacement%20Thumbnails%2Fpost%20bg.png?alt=media&token=d708b5e3-a7ee-421b-beae-3b10946678c4',
+        'bannerText': "Join our Telegram",
+        'bannerTextOn': globals.bannerTextOn,
+        'bannerURL': "https://t.me/PrismWallpapers",
+        'latestCategories': categories.toString(),
+        'currentVersion': globals.currentAppVersion.toString(),
+        'obsoleteVersion': globals.obsoleteAppVersion.toString(),
+        'versionDesc':
+            "Prism Premium is here, for the personalisaton lords!^*^Setups are here! Change the way of personalisation.^*^Favourites moved to profile.",
+        'topTitleText':
+            '["TOP-RATED","BEST OF COMMUNITY","FAN-FAVOURITE","TRENDING",]',
+        'premiumCollections': '["space","landscapes","mesh gradients",]',
+        'verifiedUsers':
+            '["akshaymaurya3006@gmail.com","maurya.abhay30@gmail.com",]'
+      });
+      debugPrint("Started Fetching Values from rc");
+      await remoteConfig.fetch(expiration: const Duration(hours: 6));
+      await remoteConfig.activateFetched();
+      debugPrint("Fetched Values from rc");
+      globals.topImageLink = remoteConfig.getString('topImageLink');
+      globals.bannerText = remoteConfig.getString('bannerText');
+      globals.bannerTextOn = remoteConfig.getString('bannerTextOn');
+      globals.bannerURL = remoteConfig.getString('bannerURL');
+      globals.obsoleteAppVersion = remoteConfig.getString('obsoleteVersion');
+      var verifiedU = remoteConfig.getString('verifiedUsers');
+      verifiedU = verifiedU.replaceAll('"', '');
+      verifiedU = verifiedU.replaceAll("[", "");
+      verifiedU = verifiedU.replaceAll(",]", "");
+      globals.verifiedUsers = verifiedU.split(",");
+      var premiumC = remoteConfig.getString('premiumCollections');
+      premiumC = premiumC.replaceAll('"', '');
+      premiumC = premiumC.replaceAll("[", "");
+      premiumC = premiumC.replaceAll(",]", "");
+      globals.premiumCollections = premiumC.split(",");
+      var text = remoteConfig.getString('topTitleText');
+      text = text.replaceAll('"', '');
+      text = text.replaceAll("[", "");
+      text = text.replaceAll(",]", "");
+      globals.topTitleText = text.split(",");
+      globals.topTitleText.shuffle();
+      final cList = [];
+      var tempVar = remoteConfig
+          .getString('latestCategories')
+          .replaceAll('[', "")
+          .replaceAll(']', "")
+          .split("},");
+      tempVar = tempVar.sublist(0, tempVar.length - 1);
+      tempVar.forEach((element) {
+        cList.add(element.split('"name": "')[1].split('",')[0].toString());
+        categories[tempVar.indexOf(element)] = json.decode("$element}");
+      });
+      debugPrint(cList.toString());
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   void checkNotch(BuildContext context) {
