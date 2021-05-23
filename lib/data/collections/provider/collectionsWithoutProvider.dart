3ecd958ba8c7ffb072880dfc6321958c30ee0d02
Abhay Maurya <maurya.abhay30@gmail.com>
2021-05-23 15:24:37 +0530

    add new getcollections method

--- lib/data/collections/provider/collectionsWithoutProvider.dart
+++ lib/data/collections/provider/collectionsWithoutProvider.dart
@@ -1,144 +1,26 @@
-import 'dart:math';
 import 'package:Prism/routes/router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
-import 'package:hive/hive.dart';
-import 'package:intl/intl.dart';
 
 final FirebaseFirestore databaseReference = FirebaseFirestore.instance;
-List? wallpapersForCollections;
-late Set collectionNames;
-Map? collections;
-Map? wall;
-Future<Map?> getCollections() async {
+List? collections;
+
+Future<List?> getCollections() async {
   if (navStack.last == "Home") {
-    final box = Hive.box('collections');
-    // debugPrint(box.get('collections').toString());
-    if ((box.get('date') !=
-            DateFormat("yy-MM-dd").format(
-              DateTime.now(),
-            )) ||
-        (box.get('collections') == null) ||
-        (box.get('collections').toString() == "[]")) {
-      wallpapersForCollections = [];
-      collectionNames = {};
-      collections = {};
-      await databaseReference
-          .collection("walls")
-          .where('review', isEqualTo: true)
-          .orderBy("createdAt", descending: true)
-          .get()
-          .then((value) {
-        wallpapersForCollections = [];
-        collectionNames = {};
-        collections = {};
-        debugPrint("Data Fetched");
-        for (final DocumentSnapshot f in value.docs) {
-          Map<String, dynamic> map;
-          map = f.data()!;
-          map['createdAt'] = map['createdAt'].toString();
-          if (map['collections'] != null) wallpapersForCollections!.add(map);
-        }
-        debugPrint("Data added to list");
-        for (final wall in wallpapersForCollections!) {
-          for (final collectionName in wall['collections']) {
-            if (!collectionNames.contains(collectionName)) {
-              collectionNames.add(collectionName);
-            }
-          }
-        }
-
-        final r = Random();
-        List randomList;
-        randomList = [];
-        var count = 0;
-        for (var i = 0; i < 10; i++) {
-          randomList.add(r.nextInt(wallpapersForCollections!.length));
-        }
-        for (final wall in wallpapersForCollections!) {
-          for (final collectionName in wall['collections']) {
-            if (!collections!.containsKey(collectionName)) {
-              collections![collectionName] = [];
-              collections![collectionName].add(wall);
-            } else {
-              collections![collectionName].add(wall);
-            }
-            if (count == 0) {
-              collections!["random"] = [];
-              collectionNames.add("random");
-            }
-
-            if (randomList.contains(count)) {
-              collections!["random"].add(wall);
-            }
-            count++;
-          }
-        }
-        debugPrint("Data grouped");
-        debugPrint(collectionNames.toString());
-
-        box.delete('Collections');
-        if (collections != {}) {
-          box.put('collections', wallpapersForCollections);
-          debugPrint("Collections saved");
-          box.put(
-            'date',
-            DateFormat("yy-MM-dd").format(
-              DateTime.now(),
-            ),
-          );
-          debugPrint(collectionNames.length.toString());
-        } else {
-          debugPrint("Not connected to Internet");
-          collectionNames = {};
-          collections = {};
-        }
-      }).catchError((e) {
-        debugPrint(e.toString());
-        debugPrint("data done with error");
-      });
-    } else {
-      debugPrint("Collections : Data Fetched from cache");
-      wallpapersForCollections = [];
-      collectionNames = {};
-      collections = {};
-      wallpapersForCollections = box.get('collections') as List?;
-      for (final wall in wallpapersForCollections!) {
-        for (final collectionName in wall['collections']) {
-          if (!collectionNames.contains(collectionName)) {
-            collectionNames.add(collectionName);
-          }
-        }
+    debugPrint("Fetching collections!");
+    collections = [];
+    await databaseReference
+        .collection("collections")
+        .orderBy("lastEditTime", descending: true)
+        .get()
+        .then((value) {
+      for (final doc in value.docs) {
+        collections!.add(doc.data());
       }
-
-      final r = Random();
-      List randomList;
-      randomList = [];
-      var count = 0;
-      for (var i = 0; i < 10; i++) {
-        randomList.add(r.nextInt(wallpapersForCollections!.length));
-      }
-      for (final wall in wallpapersForCollections!) {
-        for (final collectionName in wall['collections']) {
-          if (!collections!.containsKey(collectionName)) {
-            collections![collectionName] = [];
-            collections![collectionName].add(wall);
-          } else {
-            collections![collectionName].add(wall);
-          }
-          if (count == 0) {
-            collections!["random"] = [];
-            collectionNames.add("random");
-          }
-
-          if (randomList.contains(count)) {
-            collections!["random"].add(wall);
-          }
-          count++;
-        }
-      }
-      debugPrint(collectionNames.length.toString());
-    }
+    }).catchError((e) {
+      debugPrint(e.toString());
+      debugPrint("data done with error");
+    });
   } else {
     debugPrint("Refresh blocked");
   }
