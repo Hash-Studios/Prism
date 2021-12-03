b7bb66e4f67d25dffc65e38a5a6ad62e547d6fb1
LiquidatorCoder <maurya.abhay30@gmail.com>
2021-12-03 18:00:54 +0530

    add material 3 nav bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:prism/controllers/theme_controller.dart';
 import 'package:prism/services/logger.dart';
+import 'package:prism/widgets/navigation_bar.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -26,53 +28,69 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final darkAppBarContents =
+        Theme.of(context).scaffoldBackgroundColor.computeLuminance() > 0.5;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Prism'),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarIconBrightness:
+                darkAppBarContents ? Brightness.dark : Brightness.light),
+        title: Text(
+          'Prism',
+          style: TextStyle(
+            color: darkAppBarContents
+                ? Theme.of(context).bottomNavigationBarTheme.unselectedItemColor
+                : Theme.of(context).appBarTheme.titleTextStyle?.color,
+          ),
+        ),
         actions: [
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.search),
+            color: darkAppBarContents
+                ? Theme.of(context).bottomNavigationBarTheme.unselectedItemColor
+                : Theme.of(context).appBarTheme.titleTextStyle?.color,
           ),
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.filter_list),
+            color: darkAppBarContents
+                ? Theme.of(context).bottomNavigationBarTheme.unselectedItemColor
+                : Theme.of(context).appBarTheme.titleTextStyle?.color,
           ),
         ],
       ),
-      bottomNavigationBar: SizedBox(
-        height: 56 + 8,
-        width: MediaQuery.of(context).size.width,
-        child: BottomNavigationBar(
-          selectedFontSize: 12,
-          showUnselectedLabels: true,
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.image),
-              icon: Icon(Icons.image_outlined),
-              label: 'Walls',
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.imagesearch_roller),
-              icon: Icon(Icons.imagesearch_roller_outlined),
-              label: 'Setups',
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.notifications),
-              icon: Icon(Icons.notifications_outlined),
-              label: 'Notifications',
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.person),
-              icon: Icon(Icons.person_outlined),
-              label: 'Profile',
-            ),
-          ],
-        ),
+      bottomNavigationBar: NavigationBar(
+        backgroundColor:
+            Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.image),
+            icon: Icon(Icons.image_outlined),
+            label: 'Walls',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.imagesearch_roller),
+            icon: Icon(Icons.imagesearch_roller_outlined),
+            label: 'Setups',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.notifications),
+            icon: Icon(Icons.notifications_outlined),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.person),
+            icon: Icon(Icons.person_outlined),
+            label: 'Profile',
+          ),
+        ],
       ),
       body: Center(
         child: Card(
+          margin: const EdgeInsets.all(16),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
