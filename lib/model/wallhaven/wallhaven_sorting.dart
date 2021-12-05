2c56c9795838e73004ac6a5886a5c524054c7ba1
LiquidatorCoder <maurya.abhay30@gmail.com>
2021-12-05 18:28:29 +0530

    update wallhaven sorting enum

--- lib/model/wallhaven/wallhaven_sorting.dart
+++ lib/model/wallhaven/wallhaven_sorting.dart
@@ -11,4 +11,10 @@ extension ParseToStringS on Sorting {
   String toShortString() {
     return toString().split('.').last.replaceAll('dateAdded', 'date_added');
   }
+
+  String toText() {
+    final text =
+        toString().split('.').last.replaceAll('dateAdded', 'Date Added');
+    return text[0].toUpperCase() + text.substring(1);
+  }
 }
