d7fcb9879bbd488622e3e3f45c21efa52ab5c06c
codenameakshay <akshaymaurya3006@gmail.com>
2021-12-31 18:43:12 +0530

    add color utils

---
+++ lib/utils/color_utils.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+Color darken2(Color color, [double amount = .1]) {
+  assert(amount >= 0 && amount <= 1);
+
+  final hsl = HSLColor.fromColor(color);
+  final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+
+  return hslDark.toColor();
+}
+
+Color lighten2(Color color, [double amount = .1]) {
+  assert(amount >= 0 && amount <= 1);
+
+  final hsl = HSLColor.fromColor(color);
+  final hslLight = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+
+  return hslLight.toColor();
+}
+
+Color invert2(Color color) {
+  final hsl = HSLColor.fromColor(color);
+  final hslInvert = hsl.withLightness(1 - hsl.lightness);
+
+  return hslInvert.toColor();
+}
+
+extension DarkLight on Color {
+  Color get darken10 => darken2(this);
+  Color get lighten10 => lighten2(this);
+  Color get darken30 => darken2(this, .3);
+  Color get lighten30 => lighten2(this, .3);
+  Color get darken50 => darken2(this, .5);
+  Color get lighten50 => lighten2(this, .5);
+  Color get darken70 => darken2(this, .7);
+  Color get lighten70 => lighten2(this, .7);
+  Color get darken90 => darken2(this, .9);
+  Color get lighten90 => lighten2(this, .9);
+  Color get invert => invert2(this);
+}
