7272b34d3483a60edae782d6c1ab16a2b802ae3a
LiquidatorCoder <maurya.abhay30@gmail.com>
2026-03-16 12:42:46 +0530

    fix: minor error fix

--- lib/features/onboarding_v2/src/theme/onboarding_assets.dart
+++ lib/features/onboarding_v2/src/theme/onboarding_assets.dart
@@ -2,5 +2,5 @@ class OnboardingAssets {
   const OnboardingAssets._();
 
   static const String wallpaperPrimary = 'assets/images/onboarding_bg_primary.webp';
-  static const String wallpaperFinal = 'assets/images/onboarding_bg_final.webp';
+  static const String wallpaperFinal = 'assets/images/onboarding_bg_primary.webp';
 }
